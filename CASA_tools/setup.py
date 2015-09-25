5ec1df8b3ba5b529aa2c68360e22f5180406dadd
e-koch <koch.eric.w@gmail.com>
2015-09-25 11:15:53 -0600

    Make little package for easy use

---
+++ CASA_tools/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='CASA_tools',
+          version='0.0.0',
+          description='My tools for CASA.',
+          author='Eric Koch',
+          author_email='koch.eric.w@gmail.com',
+          url='http://github.com/e-koch/VLA_Lband/CASA_tools',
+          packages=['casa_tools'])
+

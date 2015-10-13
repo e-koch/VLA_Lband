5c08f299c0022a2d94c77dbe9f91a5fb3dec5b4d
e-koch <koch.eric.w@gmail.com>
2015-10-13 17:14:04 -0600

    Define casa tasks that fail with python errors

---
+++ CASA_tools/casa_tools/mytools.py
@@ -0,0 +1,28 @@
+
+'''
+Define my own version of CASA tools that return python errors if they fail.
+'''
+
+from tasks import split, uvsub, concat, clean, rmtables
+
+from .graceful_error_catch import catch_fail
+
+
+def mysplit(**kwargs):
+    return catch_fail(split, **kwargs)
+
+
+def myuvsub(**kwargs):
+    return catch_fail(uvsub, **kwargs)
+
+
+def myconcat(**kwargs):
+    return catch_fail(concat, **kwargs)
+
+
+def myclean(**kwargs):
+    return catch_fail(clean, **kwargs)
+
+
+def myrmtables(**kwargs):
+    return catch_fail(rmtables, **kwargs)

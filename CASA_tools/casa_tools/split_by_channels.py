69f2fb8e20ec0e74ac568eb1f8bbf0a0a075008c
e-koch <koch.eric.w@gmail.com>
2015-10-14 10:24:43 -0600

    Convert CASA spw string to slice objecty

--- CASA_tools/casa_tools/split_by_channels.py
+++ CASA_tools/casa_tools/split_by_channels.py
@@ -22,7 +22,8 @@ def split_by_channel(vis, nchan=-1, start=1, spw='0',
     tb.close()
 
     # Pick out the spw specified
-    total_nchan = nchan_list[spw]
+    spw_slicer = get_slice_obj(spw)
+    total_nchan = nchan_list[spw_slicer]
 
     # Now create a list of the channels to use
     if nchan == -1:
@@ -40,3 +41,14 @@ def split_by_channel(vis, nchan=-1, start=1, spw='0',
         mymstransform(vis=vis, outputvis=channel_vis, spw=spw,
                       regridms=True, width=1, nchan=1, start=chan,
                       restfreq=restfreq)
+
+
+def get_slice_obj(slicearg):
+    '''
+    Given a string of the form "N:I:J", where N, I, J are integers,
+    return a slice object: slice(N, I, J).
+
+    http://stackoverflow.com/questions/680826/python-create-slice-object-from-string
+    '''
+    slice_ints = tuple([int(n) for n in slicearg.split(':')])
+    return apply(slice, slice_ints)

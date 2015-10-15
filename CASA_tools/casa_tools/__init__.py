b1d6615cf2ea8bdfe559fcb4b19d8b3d3730b863
e-koch <koch.eric.w@gmail.com>
2015-10-15 09:39:59 -0600

    Add imports

--- CASA_tools/casa_tools/__init__.py
+++ CASA_tools/casa_tools/__init__.py
@@ -2,3 +2,7 @@
 from graceful_error_catch import CASAError, catch_fail
 
 from subtract_outliers import subtract_outliers
+
+from mytools import *
+
+from split_by_channels import image_split_by_channel, ms_split_by_channel

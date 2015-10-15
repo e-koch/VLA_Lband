a715b5bf41a6ee3da095d6547b6481894aa73d55
e-koch <koch.eric.w@gmail.com>
2015-10-15 14:13:40 -0600

    Reduce to 200 channels

--- 14B-088/HI/HI_channel_split.py
+++ 14B-088/HI/HI_channel_split.py
@@ -9,7 +9,7 @@ vis = "/home/ekoch/m33/14B-088/14B-088_HI_LSRK.ms.contsub"
 output_dir = "/home/ekoch/m33/14B-088/channel_ms/"
 
 start_chan = 670
-nchan = 400
+nchan = 210
 
 ms_split_by_channel(vis, nchan=nchan, start=start_chan,
                     output_dir=output_dir, datacolumn='DATA')

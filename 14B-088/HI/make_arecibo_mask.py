5fed1e4e6a97c6b36499390ef1b47bed07887afe
e-koch <koch.eric.w@gmail.com>
2015-10-22 17:17:33 -0600

    Script for producing the Arecibo mask

---
+++ 14B-088/HI/make_arecibo_mask.py
@@ -0,0 +1,20 @@
+
+'''
+Create the mask with the Arecibo data, that is then interpolated to the VLA
+cube and used as the CLEAN mask
+'''
+
+from astropy.io import fits
+
+
+# I should really make this a package...
+execfile("/home/eric/Dropbox/code_development/clean_masks/clean_mask_construct.py")
+
+arecibo = \
+    fits.open("/media/eric/Data_3/M33/Arecibo/M33only_jy_stokes_vrad.fits")[0]
+
+cleanmask = CleanMask(arecibo, 1, 5, iteraxis=1)
+
+cleanmask.make_mask(verbose=True, smooth=True, kern_size=6)
+
+cleanmask.save_to_fits("M33only_jy_stokes_vrad_mask_1sigma_dilated.fits")

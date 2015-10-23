cb4973909ea662abdf718e5a831806dcb0ecc821
e-koch <koch.eric.w@gmail.com>
2015-10-23 11:19:52 -0600

    Update what's needed to correct mask and model

--- 14B-088/HI/HI_correct_mask_model.py
+++ 14B-088/HI/HI_correct_mask_model.py
@@ -1,6 +1,6 @@
 
 '''
-Swap the spatial axes. Swap the spectral and stokes axes.
+\Swap the spectral and stokes axes. Needed due to issue in regridding function
 '''
 
 import sys
@@ -11,12 +11,7 @@ hdu = fits.open(sys.argv[1], mode='update')
 
 hdu[0].data = hdu[0].data.swapaxes(0, 1)
 
-hdu[0].data = hdu[0].data[:, :, :, ::-1]
-hdu[0].data = hdu[0].data[:, :, ::-1, :]
-
-hdu.flush()
-
-execfile("~/Dropbox/code_development/ewky_scripts/header_swap_axis.py")
+execfile("/home/eric/Dropbox/code_development/ewky_scripts/header_swap_axis.py")
 
 hdu[0].header = header_swapaxes(hdu[0].header, 2, 3)
 

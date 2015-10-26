414e977160ec6880735b037d953396fd8b388bc8
e-koch <koch.eric.w@gmail.com>
2015-10-26 14:03:28 -0600

    Should be silo

--- 14B-088/Lines/split_all_line_mysilo.sh
+++ 14B-088/Lines/split_all_line_mysilo.sh
@@ -5,23 +5,23 @@
 # RRLs. Skip the one at ~1.2 GHz. It sits on a lovely time-variable RFI peak...
 echo "RRLs"
 echo "1/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 2 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_HRL/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 2 target /home/eric/Silo/ /media/eric/Data_1/14B-088_HRL/
 echo "2/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 3 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_HRL/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 3 target /home/eric/Silo/ /media/eric/Data_1/14B-088_HRL/
 echo "3/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 5 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_HRL/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 5 target /home/eric/Silo/ /media/eric/Data_1/14B-088_HRL/
 echo "4/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 9 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_HRL/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 9 target /home/eric/Silo/ /media/eric/Data_1/14B-088_HRL/
 echo "5/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 10 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_HRL/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 10 target /home/eric/Silo/ /media/eric/Data_1/14B-088_HRL/
 
 # OH
 echo "OH"
 echo "6/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 4 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_OH/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 4 target /home/eric/Silo/ /media/eric/Data_1/14B-088_OH/
 echo "7/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 6 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_OH/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 6 target /home/eric/Silo/ /media/eric/Data_1/14B-088_OH/
 echo "8/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 7 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_OH/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 7 target /home/eric/Silo/ /media/eric/Data_1/14B-088_OH/
 echo "9/9"
-casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 8 target /home/eric/Jasper/ /media/eric/Data_1/14B-088_OH/
+casa -c ~/Dropbox/code_development/VLA_Lband/14B-088/Lines/split_spw_from_all_tracks.py 8 target /home/eric/Silo/ /media/eric/Data_1/14B-088_OH/

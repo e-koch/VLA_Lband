f3605179424570a31bf0935f8e74fde55a75df4b
e-koch <koch.eric.w@gmail.com>
2015-10-30 15:37:47 -0600

    Add CleanResults to package

--- CASA_tools/casa_tools/__init__.py
+++ CASA_tools/casa_tools/__init__.py
@@ -6,3 +6,5 @@ from subtract_outliers import subtract_outliers
 from mytools import *
 
 from split_by_channels import image_split_by_channel, ms_split_by_channel
+
+from extract_from_log import CleanResults

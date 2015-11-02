c3c163066edb9e527a558d6414ef12969d1a4790
e-koch <koch.eric.w@gmail.com>
2015-11-02 10:13:03 -0700

    Add result collector to astropy table ability

--- CASA_tools/casa_tools/extract_from_log.py
+++ CASA_tools/casa_tools/extract_from_log.py
@@ -20,15 +20,36 @@ err = r"ERROR\s"
 numbers = r"[-+]?\d*\.\d+|\d+"
 
 
-def load_log(logfile):
+def collect_clean_results(log_files, filename=None, format='ascii.csv',
+                          show_in_browser=False):
     '''
-    Load the lines of a log file in.
+    Loop through the list of given log files, extract results from the clean
+    calls, and save as a csv file.
     '''
+    results_dict = {"Name": [],
+                    "Reached Threshold": [],
+                    "Max Residual": [],
+                    "Iterations": [],
+                    "Time Elapsed": []}
 
-    with open(logfile) as f:
-        lines = f.readlines()
+    for log in log_files:
+        results = CleanResults(log)
+        results.run_all()
 
-    return lines
+        results_dict["Name"].append(log.rstrip(".log"))
+        results_dict["Reached Threshold"].append(results.finished)
+        results_dict["Max Residual"].append(results.max_residuals)
+        results_dict["Iterations"].append(results.niters)
+        results_dict["Time Elapsed"].append(results.time_elapsed)
+
+    # Now gather into a table.
+    t = Table(results_dict.values(), names=results_dict.keys())
+
+    if filename is not None:
+        t.write(filename, format=format)
+
+    if show_in_browser:
+        t.show_in_browser()
 
 
 class CleanResults(object):
@@ -335,6 +356,17 @@ class CleanResults(object):
                     str(self.max_residuals[i])+"\n  Iterations: " + \
                     str(self.niters[i])+"\n  Time Elapsed: " + \
                     str(self.time_elapsed[i].round(3))
+
+
+def load_log(logfile):
+    '''
+    Load the lines of a log file in.
+    '''
+
+    with open(logfile) as f:
+        lines = f.readlines()
+
+    return lines
 
 
 def fill_in_slice(view, list_len):

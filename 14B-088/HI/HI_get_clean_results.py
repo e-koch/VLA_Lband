48bc633eaf0748ab6ec37fd7d5a2f67a63d28d0f
e-koch <koch.eric.w@gmail.com>
2015-11-02 12:38:11 -0700

    Order channels

--- 14B-088/HI/HI_get_clean_results.py
+++ 14B-088/HI/HI_get_clean_results.py
@@ -6,8 +6,10 @@ Extracts info from the CLEAN logs in separate channels.
 import sys
 import glob
 import os
+import numpy as np
 
-from casa_tools import collect_clean_results
+# from casa_tools import collect_clean_results
+execfile("/home/eric/Dropbox/code_development/VLA_Lband/CASA_tools/casa_tools/extract_from_log.py")
 
 
 path_to_results = sys.argv[1]
@@ -18,12 +20,36 @@ show_in_browser = True if sys.argv[3] == "True" else False
 # ie. path_to_results/channel_$i/
 channel_direcs = glob.glob(os.path.join(path_to_results, "channel_*"))
 
+# Sort by the channel number
+min_chan = 10000
+max_chan = 0
+
+for channel in channel_direcs:
+    # Last 3 characters
+    chan_num = int(channel.split("_")[-1])
+    if chan_num < min_chan:
+        min_chan = chan_num
+
+    if chan_num > max_chan:
+        max_chan = chan_num
+
+ordered_chans = []
+for chan in xrange(min_chan, max_chan+1):
+    i = 0
+    while True:
+        chan_num = int(channel_direcs[i].split("_")[-1])
+        if chan_num == chan:
+            ordered_chans.append(channel_direcs[i])
+            break
+        i += 1
+
+channel_direcs = ordered_chans
+
 # Now grab the logs
 log_files = []
 for channel in channel_direcs:
     log_file = glob.glob(os.path.join(channel, "casa*.log"))
-
-    if not log_files:
+    if not log_file:
         print("Cannot find log file in "+channel)
         continue
 

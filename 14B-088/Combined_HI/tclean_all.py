5cad2bf5b13bcd2d69fece1dbe12c6414f18e4d1
e-koch <koch.eric.w@gmail.com>
2015-11-10 13:17:45 -0700

    tclean call

---
+++ 14B-088/Combined_HI/tclean_all.py
@@ -0,0 +1,21 @@
+
+'''
+Using tclean to image the regridded 14B-088 HI data and the archival data
+together.
+'''
+
+from tasks import tclean
+
+vis = ["/home/ekoch/scratch/combined/14B-088_HI_LSRK_AT0206_regrid.ms.contsub",
+       "/home/ekoch/scratch/combined/M33_b_c_LSRK.ms"]
+imagename = "/home/ekoch/scratch/combined/M33_AT0206_14B-088_HI.clean"
+model = "/home/ekoch/scratch/combined/M33_model.image"
+mask = "/home/ekoch/scratch/combined/M33_newmask.image"
+
+tclean(vis=vis, imagename=imagename, field="M33*", imsize=[4096, 4096],
+       cell="1.5arcsec", phasecenter="J2000 01h33m50.904 +30d39m35.79",
+       restfreq="1420.40575177MHz", startmodel=model, specmode='cube',
+       nchan=255, start=1, width=1, veltype='radio', gridder='mosaic',
+       pblimit=0.2, deconvolver="multiscale", scales=[0, 4, 8, 20, 40, 80],
+       weighting='natural', niter=80000, threshold="2mJy/beam",
+       cycleniter=5000, mask=mask, parallel=True)

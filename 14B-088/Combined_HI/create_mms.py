86a3db0b7a5edf5b7f5bb91439857a077ba24c4e
e-koch <koch.eric.w@gmail.com>
2015-11-10 12:56:56 -0700

    Increase numsubms to # procs that will be used for cleaning

--- 14B-088/Combined_HI/create_mms.py
+++ 14B-088/Combined_HI/create_mms.py
@@ -9,11 +9,11 @@ vis = "/home/ekoch/scratch/combined/14B-088_HI_LSRK_AT0206_regrid.ms.contsub"
 outputvis = \
     "/home/ekoch/scratch/combined/14B-088_HI_LSRK_AT0206_regrid.ms.contsub.mms"
 
-partition(vis=vis, outputvis=outputvis)
+partition(vis=vis, outputvis=outputvis, numsubms=60)
 
 
 vis = "/home/ekoch/scratch/combined/M33_b_c_LSRK.ms"
 outputvis = \
     "/home/ekoch/scratch/combined/M33_b_c_LSRK.mms"
 
-partition(vis=vis, outputvis=outputvis)
+partition(vis=vis, outputvis=outputvis, numsubms=60)

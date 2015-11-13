7a7c4c9c640a1f5e049be41e882c8c777700facf
e-koch <koch.eric.w@gmail.com>
2015-11-13 13:08:56 -0700

    exportfits that attachs the common beam to the header

--- CASA_tools/casa_tools/mytools.py
+++ CASA_tools/casa_tools/mytools.py
@@ -3,9 +3,13 @@
 Define my own version of CASA tools that return python errors if they fail.
 '''
 
-from tasks import split, uvsub, concat, clean, rmtables, mstransform
+from tasks import split, uvsub, concat, clean, rmtables, mstransform, \
+    exportfits
 
 from taskinit import ia
+
+from astropy.io import fits
+import astropy.units as u
 
 from .graceful_error_catch import catch_fail
 
@@ -50,3 +54,38 @@ def myclean(**kwargs):
             pass
 
     return catch_fail(clean, **kwargs)
+
+
+def myexportfits(common_beam=True, **kwargs):
+    '''
+    Version of exportfits that returns a Python error when it fails.
+    Also attachs a common beam to the fits header.
+    '''
+
+    catch_fail(exportfits, **kwargs)
+
+    # The above throws an error if exportfits fails.
+    # Now open the FITS file and attach a beam.
+
+    ia.open(kwargs['imagename'])
+
+    com_beam = ia.commonbeam()
+
+    ia.close()
+
+    bmaj = com_beam['major']['value'] * \
+        u.Unit(com_beam['major']['unit']).to(u.deg)
+    bmin = com_beam['minor']['value'] * \
+        u.Unit(com_beam['minor']['unit']).to(u.deg)
+    bpa = com_beam['pa']['value'] * u.Unit(com_beam['pa']['unit']).to(u.deg)
+
+    filename = kwargs['fitsimage']
+
+    output_fits = fits.open(filename, mode='update')
+
+    output_fits[0].header.update({"BMAJ": bmaj,
+                                  "BMIN": bmin,
+                                  "BPA": bpa})
+
+    output_fits.flush()
+    output_fits.close()

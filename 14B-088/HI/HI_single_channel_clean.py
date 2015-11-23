b497c35a313be4b49f9071e5cee0f2b54a8900db
e-koch <koch.eric.w@gmail.com>
2015-11-23 13:52:28 -0700

    Raise niter ceiling

--- 14B-088/HI/HI_single_channel_clean.py
+++ 14B-088/HI/HI_single_channel_clean.py
@@ -24,7 +24,7 @@ myclean(vis=vis, imagename=out_root+'.clean', field='M33*',
         mode='channel', width=1, nchan=1, start=1,
         cell='3arcsec', multiscale=[0, 4, 8, 20, 40, 80],
         threshold='1.5mJy/beam', imagermode='mosaic', gain=0.1,
-        imsize=[2560, 2560], weighting='natural', robust=0.0, niter=100000,
+        imsize=[2560, 2560], weighting='natural', robust=0.0, niter=200000,
         pbcor=True, minpb=0.2, interpolation='linear', usescratch=False,
         phasecenter='J2000 01h33m50.904 +30d39m35.79', veltype='radio',
         modelimage=model, mask=mask)

8bcb7ffda825b70fbb71b9e110d82e06214b8758
e-koch <koch.eric.w@gmail.com>
2015-12-04 14:07:50 -0700

    Channel split jobs for combined cleaning

---
+++ 14B-088/Combined_HI/14B-088_channel_split.py
@@ -0,0 +1,15 @@
+
+'''
+Split the regridded HI data from 14B-088 into individual channels.
+'''
+
+from casa_tools import ms_split_by_channel
+
+vis = "/global/scratch/ekoch/combined/14B-088_HI_LSRK_AT0206_regrid.ms.contsub"
+output_dir = "/global/scratch/ekoch/combined/14B-088_channel_ms/"
+
+start_chan = 10
+nchan = 205
+
+ms_split_by_channel(vis, nchan=nchan, start=start_chan,
+                    output_dir=output_dir, datacolumn='DATA')
---
+++ 14B-088/Combined_HI/AT0206_channel_split.py
@@ -0,0 +1,15 @@
+
+'''
+Split the HI data from AT0206 into individual channels.
+'''
+
+from casa_tools import ms_split_by_channel
+
+vis = "/global/scratch/ekoch/combined/M33_b_c_LSRK.ms"
+output_dir = "/global/scratch/ekoch/combined/AT0206_channel_ms/"
+
+start_chan = 10
+nchan = 205
+
+ms_split_by_channel(vis, nchan=nchan, start=start_chan,
+                    output_dir=output_dir, datacolumn='DATA')

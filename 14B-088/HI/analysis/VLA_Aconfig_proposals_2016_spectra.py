5834ed9a217ea4358038417473d03c9e83840574
e-koch <koch.eric.w@gmail.com>
2015-12-21 19:42:19 -0800

    Clip velocity axis

--- 14B-088/HI/analysis/VLA_Aconfig_proposals_2016_spectra.py
+++ 14B-088/HI/analysis/VLA_Aconfig_proposals_2016_spectra.py
@@ -17,15 +17,15 @@ cube = SpectralCube.read(os.path.join(path, "M33_14B-088_HI.clean.image.fits"),
                          mode='denywrite')
 
 # Interesting positions by-eye
-posns = [["South Arm 1", 1383, 1129],
-         ["South Arm 2", 1427, 1099],
-         ["South Arm 3", 1429, 1109],
-         ["South Arm 4", 1413, 1106],
-         ["South Arm 5", 1370, 1186],
-         ["NGC 604 1", 1106, 1419],
-         ["NGC 604 2", 1104, 1432],
-         ["NGC 604 3", 1080, 1439],
-         ["NGC 604 4", 1078, 1444]]
+posns = [["South Arm 1", 1383, 1129, -210000, -75000],
+         ["South Arm 2", 1427, 1099, -210000, -75000],
+         ["South Arm 3", 1429, 1109, -210000, -75000],
+         ["South Arm 4", 1413, 1106, -210000, -75000],
+         ["South Arm 5", 1370, 1186, -210000, -75000],
+         ["NGC 604 1", 1106, 1419, -280000, -150000],
+         ["NGC 604 2", 1104, 1432, -280000, -150000],
+         ["NGC 604 3", 1080, 1439, -280000, -150000],
+         ["NGC 604 4", 1078, 1444, -280000, -150000]]
 
 for posn in posns:
     p.title(posn[0]+" at "+str(posn[1])+","+str(posn[2]))
@@ -34,5 +34,8 @@ for posn in posns:
                      "M33_14B-088_HI.clean.image." +
                      posn[0].replace(" ", "_").lower()+"_" +
                      str(posn[1])+"_"+str(posn[2])+".pdf")
-    cube[:, posn[1], posn[2]].quicklook(filename=filename)
+    cube[:, posn[2], posn[1]].quicklook()
+    p.plot([posn[3], posn[4]], [0.0]*2, 'k--')
+    p.xlim([posn[3], posn[4]])
+    p.savefig(filename)
     p.clf()

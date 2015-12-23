5d514ddafd475e9daa18d06f6472b06e4c138322
e-koch <koch.eric.w@gmail.com>
2015-12-23 13:08:06 -0800

    Add in S Arm VLA only

--- 14B-088/HI/analysis/VLA_Aconfig_proposals_2016_spectra.py
+++ 14B-088/HI/analysis/VLA_Aconfig_proposals_2016_spectra.py
@@ -21,9 +21,12 @@ arec_cube = "/media/eric/Data_3/M33/Arecibo/14B-088_items/M33_14B-088_HI_model.f
 # arec_cube = "/srv/astro/surveys/m33/hi/M33only.fits"
 
 arecibo = SpectralCube.read(arec_cube, mode='denywrite')
-# arecibo = arecibo[24:1202]
 
 # arec_spec = arecibo[:, 170, 132]
+
+# I've imaged a portion of the Southern Arm w/o Arecibo.
+vla_path = "/media/eric/MyRAID/M33/14B-088/HI/imaging/south_arm_800_1200.image.fits"
+sarm_vla = SpectralCube.read(vla_path, mode='denywrite')
 
 # Interesting positions by-eye
 posns = [["South Arm 1", 1383, 1129, -210000, -75000],
@@ -36,10 +39,12 @@ posns = [["South Arm 1", 1383, 1129, -210000, -75000],
          ["NGC 604 3", 1080, 1439, -280000, -150000],
          ["NGC 604 4", 1078, 1444, -280000, -150000]]
 
+p.ioff()
+
 for posn in posns:
     p.title(posn[0]+" at "+str(posn[1])+","+str(posn[2]))
     filename = \
-        os.path.join(path,
+        os.path.join("/home/eric/Dropbox/M33/",
                      "M33_14B-088_HI.clean.image." +
                      posn[0].replace(" ", "_").lower()+"_" +
                      str(posn[1])+"_"+str(posn[2])+".pdf")
@@ -47,16 +52,21 @@ for posn in posns:
     spec_conv = spec.to(u.K, equivalencies=cube.beam.jtok_equiv(1420.40575177*u.MHz))
     # Temp fix for WCS not being kept after unit conversion
     spec_conv._wcs = spec.wcs
-    spec_conv.quicklook(label="VLA + Arecibo")
+    spec_conv.quicklook(label="VLA + Arecibo", c='b')
     arec_spec = arecibo[:, -posn[2], -posn[1]]
     arec_spec_conv = arec_spec.to(u.K, equivalencies=arecibo.beam.jtok_equiv(1420.40575177*u.MHz))
     arec_spec_conv._wcs = arec_spec.wcs
-    arec_spec_conv.quicklook(label="Arecibo")
+    arec_spec_conv.quicklook(label="Arecibo", c='c')
     # p.plot(arec_spec_conv.spectral_axis[::-1], arec_spec_conv.value,
     #        drawstyle='steps-mid')
+    if "South" in posn[0]:
+        vla_spec = sarm_vla[:, posn[2], posn[1]]
+        vla_spec_conv = vla_spec.to(u.K, equivalencies=sarm_vla.beam.jtok_equiv(1420.40575177*u.MHz))
+        vla_spec_conv._wcs = vla_spec.wcs
+        vla_spec_conv.quicklook(label="VLA", c='r')
     p.plot([posn[3], posn[4]], [0.0]*2, 'k--')
-    # p.xlim([posn[3], posn[4]])
+    p.xlim([posn[3], posn[4]])
     p.legend(loc='best')
-    # p.savefig(filename)
+    p.savefig(filename)
     p.show()
     p.clf()

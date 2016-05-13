a33ee07fa08f79e02e70ec3c57aa77e393fba310
e-koch <koch.eric.w@gmail.com>
2016-05-13 17:07:34 -0600

    Add sigma estimation in each bin

--- 14B-088/HI/analysis/HI_radial_profile.py
+++ 14B-088/HI/analysis/HI_radial_profile.py
@@ -24,6 +24,7 @@ def radial_profile(gal, cube, dr=100 * u.pc, mom0=None,
     inneredge = np.linspace(0, max_rad, nbins)
     outeredge = np.linspace(0 + dr, max_rad + dr, nbins)
     sdprof = np.zeros(nbins)
+    sdprof_sigma = np.zeros(nbins)
     radprof = np.zeros(nbins)
     for ctr, (r0, r1) in enumerate(zip(inneredge,
                                        outeredge)):
@@ -31,29 +32,39 @@ def radial_profile(gal, cube, dr=100 * u.pc, mom0=None,
         idx = np.logical_and(radius >= r0, radius < r1)
         sdprof[ctr] = np.nansum(mom0[idx].value) / \
             np.sum(np.isfinite(mom0[idx].value))
+        sdprof_sigma[ctr] = \
+            np.sqrt(np.nansum((mom0[idx].value - sdprof[ctr])**2.) / \
+                    np.sum(np.isfinite(mom0[idx].value)))
         radprof[ctr] = np.nanmean(radius[idx])
 
     # Re-apply some units
     radprof = radprof * u.kpc
     sdprof = sdprof * mom0.unit
+    sdprof = sdprof_sigma * mom0.unit
 
     # Correct for the los inclinations
     sdprof *= np.cos(gal.inclination)
+    sdprof_sigma *= np.cos(gal.inclination)
 
     # If in Jy/bm, convert to K.
     if cube.unit.is_equivalent(u.Jy):
         # The beam units are sort of implied
         sdprof = sdprof.to(u.Jy * u.km / u.s)
+        sdprof_sigma = sdprof_sigma.to(u.Jy * u.km / u.s)
         # Now convert to K
         if restfreq is not None:
             sdprof = sdprof * mom0.meta["beam"].jtok(restfreq)
+            sdprof_sigma = sdprof_sigma * mom0.meta["beam"].jtok(restfreq)
         else:
             sdprof = sdprof * mom0.meta["beam"].jtok(mom0.header["RESTFREQ"])
+            sdprof_sigma = sdprof_sigma * \
+                mom0.meta["beam"].jtok(mom0.header["RESTFREQ"])
 
     if mass_conversion is not None:
         sdprof = sdprof * mass_conversion
+        sdprof_sigma = sdprof_sigma * mass_conversion
 
-    return radprof, sdprof
+    return radprof, sdprof, sdprof_sigma
 
 if __name__ == "__main__":
     from galaxies import Galaxy
@@ -75,11 +86,11 @@ if __name__ == "__main__":
     # cube = cube[400:600]
 
     # Create a radial profile of HI
-    rs, sd = radial_profile(g, cube)
+    rs, sd, sd_sigma = radial_profile(g, cube)
 
     # Add in creating a radial profile of the archival and the Arecibo
     # Also CO? I guess these should be on the same grid/resolution...
 
-    p.plot(rs, sd, "bD-")
+    p.errorbar(rs, sd, "bD-", yerr=sd_sigma)
     p.ylabel(r"$\Sigma$ (M$_{\odot}$ K$^{-1}$km$^{-1}$s)")
     p.xlabel(r"R (kpc)")

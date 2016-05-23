68a22bfab8db1b9412b25b32894dd575ff4c1696
e-koch <koch.eric.w@gmail.com>
2016-05-23 10:46:09 -0600

    degrees and radians matter

--- 14B-088/HI/analysis/rotation_curves/vrot_fit.py
+++ 14B-088/HI/analysis/rotation_curves/vrot_fit.py
@@ -38,6 +38,7 @@ print("vmax: {0} +/- {1}".format(pars[1], np.sqrt(pcov[1, 1])))
 print("rmax: {0} +/- {1}".format(pars[2], np.sqrt(pcov[2, 2])))
 
 mom1_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3_masked.mom1.fits"
+# mom1_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.mom1.fits"
 mom1 = \
     fits.open(os.path.join(data_path, mom1_name))
 
@@ -48,11 +49,12 @@ dist_scale = (np.pi / 180.) * 840.
 
 if make_plot:
     phys_radius = data["r"] * scale * dist_scale
-    pars[2] *= scale * dist_scale
+    plot_pars = pars.copy()
+    plot_pars[2] *= scale * dist_scale
     p.ioff()
     p.errorbar(phys_radius, data["Vt"], yerr=data['eVt'],
                fmt='D', color='b')
-    p.plot(phys_radius, vcirc(phys_radius, *pars), 'r-')
+    p.plot(phys_radius, vcirc(phys_radius, *plot_pars), 'r-')
     p.ylabel(r"V$_{\mathrm{circ}}$ (km / s)")
     p.xlabel(r"Radius (kpc)")
     p.show()
@@ -66,11 +68,12 @@ if make_rotmodel:
     pas = gal.position_angles(header=mom1[0].header).value
 
     # Convert rmax to pc
-    pars[2] *= scale * dist_scale * 1000.
+    mod_pars = pars.copy()
+    mod_pars[2] *= scale * dist_scale * 1000.
 
     # Put into m/s.
-    smooth_model = (vcirc(radii, *pars) * 1000.) * -np.cos(pas) * \
-        np.sin(gal.inclination.value)
+    smooth_model = (vcirc(radii, *mod_pars) * 1000.) * np.cos(pas) * \
+        np.sin(gal.inclination)
 
     # Shift by Vsys (m / s)
     vsys = -180610.
@@ -78,9 +81,10 @@ if make_rotmodel:
 
     # Save the smooth model.
     new_header = mom1[0].header.copy()
-    new_header["COMMENT"] = "Smooth rotation model of DISKFIT output. Uses Eq.5" \
-        " from Meidt et al. 2008."
+    new_header["COMMENT"] = "Smooth rotation model of DISKFIT output. " \
+        "Uses Eq.5 from Meidt et al. 2008."
     new_header["BUNIT"] = "m / s"
     new_hdu = fits.PrimaryHDU(smooth_model, header=new_header)
     new_hdu.writeto(os.path.join(data_path,
-                               "diskfit_noasymm_nowarp_output/rad.fitmod.fits"))
+                               "diskfit_noasymm_nowarp_output/rad.fitmod.fits"),
+                    clobber=True)

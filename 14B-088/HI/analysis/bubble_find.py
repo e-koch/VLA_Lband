fe442d2c36561a53a1bd3744ae24f217f123c1ce
e-koch <koch.eric.w@gmail.com>
2016-05-27 00:06:30 -0600

    Updates and save output

--- 14B-088/HI/analysis/bubble_find.py
+++ 14B-088/HI/analysis/bubble_find.py
@@ -20,6 +20,10 @@ cube = \
 
 sigma = sig_clip(cube[-1].value, nsig=10)
 
+# Create a cruddy linewidth map. This will need to be improved, but that can
+# be done with just the saved bubble objects
+lwidth = cube.with_mask(cube > 2 * sigma * u.Jy).linewidth()
+
 galaxy_props = {"center_coord":
                 SkyCoord(23.461667, 30.660194,
                          unit=(u.deg, u.deg), frame='fk5'),
@@ -30,23 +34,33 @@ galaxy_props = {"center_coord":
 
 scales = 3. * np.arange(1, 15, np.sqrt(2))
 
-cube = cube[300:400]
-
 # The first few channels have some emission in them. It's partially M33,
 # partially galactic HI. So estimate the noise level from the last channel
 # which is pretty much noise only. It gives 1.8 mJy/bm, which is right on.
 bub_find = BubbleFinder(cube, keep_threshold_mask=True,
-                        empty_channel=cube.shape[0] - 1,
+                        # empty_channel=cube.shape[0] - 1,  # Given above
                         sigma=sigma,
                         galaxy_props=galaxy_props,
                         distance=0.84 * u.Mpc)
 
+# Requiring minimum of 15 channels, similar to the 3 channel requirement with
+# THINGS (15 * 0.2 km/s = 3 km/s; 3 * 1.2 km/s = 3.6 km/s)
 bub_find.get_bubbles(verbose=True, overlap_frac=0.5, multiprocess=True,
                      refit=False, nsig=1.5, min_corr=0.7, min_overlap=0.8,
-                     min_channels=15, nprocesses=None, scales=scales)
+                     min_channels=15, nprocesses=None, scales=scales,
+                     cube_linewidth=lwidth)
 
+# Show the outlines of the bubbles
 bub_find.visualize_bubbles()
+
+# Save all of the bubbles
+folder = os.path.expanduser("~/MyRAID/M33/14B-088/HI/full_imaging/bubbles/")
+try:
+    os.mkdir(folder)
+except OSError:
+    pass
+bub_find.save_bubbles(folder=folder, name="M33_14B-088")
 
 catalog = bub_find.to_catalog()
 
-catalog.write_table(os.path.expanduser("~/MyRAID/M33/14B-088/full_imaging/bubble_catalog_chan_300_400.ecsv"))
+catalog.write_table(os.path.join(folder, "bubble_catalog.ecsv"))

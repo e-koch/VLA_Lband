910df4361c5ef2698cd234796a5ecf1513d9de65
e-koch <koch.eric.w@gmail.com>
2016-09-03 18:23:04 -0600

    No args for clearstat; don't need flag backup

--- 16B/pipeline4.6.0_custom/EVLA_pipe_remove_calibration.py
+++ 16B/pipeline4.6.0_custom/EVLA_pipe_remove_calibration.py
@@ -16,12 +16,12 @@ clearcal(vis=ms_name, addmodel=False)
 
 print("Clearing statwts")
 default("clearstat")
-clearstat(vis=ms_name)
+clearstat()
 
 if remove_flagging:
     print("Removing all flags.")
     default("flagdata")
-    flagdata(vis=ms_name, mode='unflag')
+    flagdata(vis=ms_name, mode='unflag', flagbackup=False)
 
 print("Finished resetting MS. Note that that data are still Hanning smoothed,"
       " if this pipeline option was chosen.")

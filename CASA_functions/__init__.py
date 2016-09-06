a12c50e1d3ac44ec8d853e640c283b7b53c54f59
e-koch <koch.eric.w@gmail.com>
2016-09-06 16:15:18 -0600

    Import get_mosaic_info

--- CASA_functions/__init__.py
+++ CASA_functions/__init__.py
@@ -10,6 +10,6 @@ except ImportError:
 from imaging_utils import set_imagermode, has_field
 
 try:
-    from imaging_utils import set_cellsize, set_imagesize
+    from imaging_utils import set_cellsize, set_imagesize, get_mosaic_info
 except ImportError:
     warn("Cannot import set_cellsize or set_imagesize")

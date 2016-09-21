cd7578a2d0499f5dd2c605b8bb93d6d908abab18
e-koch <koch.eric.w@gmail.com>
2016-09-21 15:15:32 -0600

    Distinguish between casa versions and add to PATH

--- 14B-088/HI/imaging/imaging_tests/download_casa_versions.sh
+++ 14B-088/HI/imaging/imaging_tests/download_casa_versions.sh
@@ -8,32 +8,42 @@ wget https://svn.cv.nrao.edu/casa/distro/linux/old/casapy-42.2.30986-pipe-1-64b.
 tar -zxf casapy-42.2.30986-pipe-1-64b.tar.gz
 rm casapy-42.2.30986-pipe-1-64b.tar.gz
 
-casa-4.2.2="$HOME/casapy-42.2.30986-pipe-1-64b/bin/casa"
+mv casapy-42.2.30986-pipe-1-64b/bin/casa casapy-42.2.30986-pipe-1-64b/bin/casa-4.2.2
+
+echo 'export PATH="$PATH:/home/eric/casapy-42.2.30986-pipe-1-64b/bin"' >> $HOME/.bashrc
 
 # CASA 4.3.1
 wget https://svn.cv.nrao.edu/casa/distro/linux/release/el6/casa-release-4.3.1-el6.tar.gz
 tar -zxf casa-release-4.3.1-el6.tar.gz
 rm casa-release-4.3.1-el6.tar.gz
 
-casa-4.3.1="$HOME/casa-release-4.3.1-el6/bin/casa"
+mv casa-release-4.3.1-el6/bin/casa casa-release-4.3.1-el6/bin/casa-4.3.1
+
+echo 'export PATH="$PATH:/home/eric/casa-release-4.3.1-el6/bin"' >> $HOME/.bashrc
 
 # CASA 4.4
 wget https://svn.cv.nrao.edu/casa/distro/linux/release/el6/casa-release-4.4.0-el6.tar.gz
 tar -zxf casa-release-4.4.0-el6.tar.gz
 rm casa-release-4.4.0-el6.tar.gz
 
-casa-4.4="$HOME/casa-release-4.4.0-el6/bin/casa"
+mv casa-release-4.4.0-el6/bin/casa casa-release-4.4.0-el6/bin/casa-4.4
+
+echo 'export PATH="$PATH:/home/eric/casa-release-4.4.0-el6/bin"' >> $HOME/.bashrc
 
 # CASA 4.5.3
 wget https://svn.cv.nrao.edu/casa/distro/linux/release/el6/casa-release-4.5.3-el6.tar.gz
 tar -zxf casa-release-4.5.3-el6.tar.gz
 rm casa-release-4.5.3-el6.tar.gz
 
-casa-4.5.3="$HOME/casa-release-4.5.3-el6/bin/casa"
+mv casa-release-4.5.3-el6/bin/casa casa-release-4.5.3-el6/bin/casa-4.5.3
+
+echo 'export PATH="$PATH:/home/eric/casa-release-4.4.0-el6/bin"' >> $HOME/.bashrc
 
 # CASA 4.6
 wget https://svn.cv.nrao.edu/casa/distro/linux/release/el6/casa-release-4.6.0-el6.tar.gz
 tar -zxf casa-release-4.6.0-el6.tar.gz
 rm casa-release-4.6.0-el6.tar.gz
 
-casa="$HOME/casa-release-4.6.0-el6/bin/casa"
+mv casa-release-4.6.0-el6/bin/casa casa-release-4.6.0-el6/bin/casa-4.6
+
+echo 'export PATH="$PATH:/home/eric/casa-release-4.4.0-el6/bin"' >> $HOME/.bashrc

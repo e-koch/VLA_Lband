12d34aa86bef6ee5cb427423167453b4f60a3d0e
e-koch <koch.eric.w@gmail.com>
2016-09-21 22:10:20 -0600

    Not all have the revision number

--- 14B-088/HI/imaging/imaging_tests/run_tests.py
+++ 14B-088/HI/imaging/imaging_tests/run_tests.py
@@ -41,7 +41,12 @@ call = call.replace("HOURS", HOURS)
 # Now loop through all combinations
 for version in versions:
     for tclean in ["T", "F"]:
-        major, minor, revision = version.split("-")[1].split('.')
+        try:
+            major, minor, revision = version.split("-")[1].split('.')
+        except ValueError:
+            major, minor = version.split("-")[1].split('.')
+            revision = 0
+
         casa_version = 100 * int(major) + 10 * int(minor) + int(revision)
 
         # Skip if on a version w/o tclean

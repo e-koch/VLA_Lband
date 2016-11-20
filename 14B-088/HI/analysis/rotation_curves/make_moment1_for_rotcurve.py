1da1197220a2196fddd412edb39561d262f7857d
e-koch <koch.eric.w@gmail.com>
2016-11-20 14:50:02 -0700

    Uncomment

--- 14B-088/HI/analysis/rotation_curves/make_moment1_for_rotcurve.py
+++ 14B-088/HI/analysis/rotation_curves/make_moment1_for_rotcurve.py
@@ -21,18 +21,18 @@ subcube = cube.subcube_from_ds9region(region)
 # Now create the moment 1 and save it. Make a linewidth one too.
 # DISKFIT has issues with float64, so convert to float32 then save
 
-# moment0_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.mom0.fits"
-# moment0 = subcube.moment0()
-# moment0.write(fourteenB_HI_data_path(moment0_name, no_check=True))
+moment0_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.mom0.fits"
+moment0 = subcube.moment0()
+moment0.write(fourteenB_HI_data_path(moment0_name, no_check=True))
 
-# moment1_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.mom1.fits"
-# moment1 = subcube.moment1().astype(np.float32)
-# moment1.header["BITPIX"] = -32
-# moment1.write(fourteenB_HI_data_path(moment1_name, no_check=True))
+moment1_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.mom1.fits"
+moment1 = subcube.moment1().astype(np.float32)
+moment1.header["BITPIX"] = -32
+moment1.write(fourteenB_HI_data_path(moment1_name, no_check=True))
 
-# lwidth_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.linewidth.fits"
-# linewidth = subcube.linewidth_sigma()
-# linewidth.write(fourteenB_HI_data_path(lwidth_name, no_check=True))
+lwidth_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.linewidth.fits"
+linewidth = subcube.linewidth_sigma()
+linewidth.write(fourteenB_HI_data_path(lwidth_name, no_check=True))
 
 # Make an array of the velocities of the peak intensities.
 peak_intens_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.3.ellip_mask.peakvels.fits"

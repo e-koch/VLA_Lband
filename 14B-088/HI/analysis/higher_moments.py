db339a3d84596c9150aea014e2c3796bab7bf8d2
e-koch <koch.eric.w@gmail.com>
2016-12-07 11:28:40 -0700

    Adjust plotting values for higher moment plots

--- 14B-088/HI/analysis/higher_moments.py
+++ 14B-088/HI/analysis/higher_moments.py
@@ -1,6 +1,7 @@
 
 from spectral_cube import SpectralCube
 from spectral_cube.lower_dimensional_structures import Projection
+from spectral_cube.cube_utils import average_beams
 import numpy as np
 import matplotlib.pyplot as p
 from astropy.io import fits
@@ -9,33 +10,41 @@ import astropy.units as u
 
 from analysis.paths import fourteenB_HI_data_path, paper1_figures_path
 from constants import (hi_freq, cube_name, moment0_name, lwidth_name,
-                       skew_name, kurt_name)
+                       skew_name, kurt_name, mask_name)
 
 '''
 Investigating skewness and kurtosis in the 14B-088 cube.
 '''
 
 cube = SpectralCube.read(fourteenB_HI_data_path(cube_name))
-mom0 = fits.open(fourteenB_HI_data_path(moment0_name))[0]
-lwidth = fits.open(fourteenB_HI_data_path(lwidth_name))[0]
+mask = fits.open(fourteenB_HI_data_path(mask_name))[0].data > 0
+cube = cube.with_mask(mask)
+
+mom0_hdu = fits.open(fourteenB_HI_data_path(moment0_name))[0]
+mom0 = Projection(mom0_hdu.data, wcs=WCS(mom0_hdu.header),
+                  unit=u.Jy * u.m / u.s)
+
+lwidth_hdu = fits.open(fourteenB_HI_data_path(lwidth_name))[0]
+lwidth = Projection(lwidth_hdu.data, wcs=WCS(lwidth_hdu.header),
+                    unit=u.m / u.s)
 
 skew_hdu = fits.open(fourteenB_HI_data_path(skew_name))[0]
-skew = Projection(skew_hdu.data, wcs=WCS(skew_hdu.header), unit=u.km / u.s)
+skew = Projection(skew_hdu.data, wcs=WCS(skew_hdu.header), unit=u.Unit(""))
 
 kurt_hdu = fits.open(fourteenB_HI_data_path(kurt_name))[0]
-kurt = Projection(kurt_hdu.data, wcs=WCS(kurt_hdu.header), unit=u.km / u.s)
+kurt = Projection(kurt_hdu.data, wcs=WCS(kurt_hdu.header), unit=u.Unit(""))
 
 
 # Make a nice 2 panel figure
 ax = p.subplot(121, projection=skew.wcs)
 ax.imshow(skew.value,
-          origin='lower', vmin=-3, vmax=3,
+          origin='lower', vmin=-2, vmax=2,
           interpolation='nearest', cmap='seismic')
 ax.set_title("Skewness")
 ax.set_ylabel("DEC (J2000)")
 ax.set_xlabel("RA (J2000)")
 ax2 = p.subplot(122, projection=kurt.wcs)
-ax2.imshow(kurt.value, vmin=0, vmax=20,
+ax2.imshow(kurt.value, vmin=1, vmax=4,
            origin='lower', interpolation='nearest', cmap='binary')
 ax2.set_title("Kurtosis")
 ax2.set_xlabel("RA (J2000)")
@@ -52,15 +61,16 @@ p.clf()
 
 # Northern HI infall: [1373, 630], [1373, 640], [1373, 650]
 nplume_positions = \
-    np.array([[1385, 603], [1385, 613], [1385, 623], [1385, 633], [1385, 643]])
+    np.array([[1375, 603], [1375, 613], [1375, 623], [1375, 633], [1375, 643]])
 
 # SArm positions
-sarm_positions = np.array([[670, 740], [660, 740], [650, 740], [640, 740],
-                           [630, 740]])
+sarm_positions = np.array([[680, 730], [670, 730], [660, 730], [650, 730],
+                           [640, 730],
+                           [630, 730]])
 
 # NGC 604
-ngc604_positions = np.array([[980, 455], [970, 455], [960, 455], [950, 455],
-                             [940, 455], [930, 455], [920, 455]])
+ngc604_positions = np.array([[980, 445], [970, 445], [960, 445], [950, 445],
+                             [940, 445], [930, 445], [920, 445]])
 
 
 # Southern Arm, NGC 604, Northern plume
@@ -75,16 +85,16 @@ names = ["sarm", "ngc604", "nplume"]
 for i, (slices, posns) in enumerate(zip(slicer, spec_posns)):
     # Moment 0 of the last few channels to highlight in-falling HI plume
     if i == 2:
-        loc_mom0 = cube[950:].moment0().hdu
+        loc_mom0 = cube[950:].moment0()
     else:
         loc_mom0 = mom0
     # Moment 0
     ax = p.subplot(221, projection=mom0[slices].wcs)
     ax.plot(posns[:, 1] - slices[1].start, posns[:, 0] - slices[0].start,
-             color="chartreuse",
+            color="chartreuse",
             marker="D", linestyle="None",
             markersize=6)
-    ax.imshow(loc_mom0.data[slices], origin='lower',
+    ax.imshow(loc_mom0[slices].value, origin='lower',
               interpolation='nearest', cmap='binary')
     ax.set_ylabel("DEC (J2000)")
     ax.set_title("Moment 0")
@@ -111,7 +121,7 @@ for i, (slices, posns) in enumerate(zip(slicer, spec_posns)):
              color="chartreuse",
              marker="D", linestyle="None",
              markersize=6)
-    ax3.imshow(skew.value[slices], vmin=-3, vmax=3,
+    ax3.imshow(skew.value[slices], vmin=-2, vmax=2,
                origin='lower',
                interpolation='nearest', cmap='seismic')
     ax3.set_ylabel("DEC (J2000)")
@@ -123,7 +133,7 @@ for i, (slices, posns) in enumerate(zip(slicer, spec_posns)):
              color="chartreuse",
              marker="D", linestyle="None",
              markersize=6)
-    ax4.imshow(kurt.value[slices], vmin=0, vmax=20,
+    ax4.imshow(kurt.value[slices], vmin=1, vmax=4,
                origin='lower', interpolation='nearest', cmap='binary')
     ax4.set_xlabel("RA (J2000)")
     lat = ax4.coords[1]
@@ -143,14 +153,14 @@ spectral_cuts = np.array([[-72, -180], [-200, -280], [-220, -315]]) * \
     u.km / u.s
 
 # Now save spectra going through the interesting regions:
-for name, posns, cuts in zip(spec_posns, spectral_cuts, names):
+for posns, cuts, name in zip(spec_posns, spectral_cuts, names):
 
     num_posns = len(posns)
     fig, axes = p.subplots(num_posns, 1, sharey=True, sharex=False,
                            figsize=(6, 10))
     for i, (posn, ax) in enumerate(zip(posns, axes)):
         spec = cube.spectral_slab(cuts[0], cuts[1])[:, posn[0], posn[1]]
-        spec = spec.to(u.K, equivalencies=cube.beam.jtok_equiv(hi_freq))
+        spec = spec.to(u.K, equivalencies=average_beams(cube.beams).jtok_equiv(hi_freq))
         velocities = cube.spectral_slab(cuts[0], cuts[1]).spectral_axis.to(u.km / u.s).value
         ax.plot(velocities, spec.value, 'b-', drawstyle='steps-mid')
         if i < num_posns - 1:
@@ -171,8 +181,8 @@ for name, posns, cuts in zip(spec_posns, spectral_cuts, names):
     fig.text(0.04, 0.5, 'Intensity (K)', va='center', rotation='vertical')
     p.draw()
 
-    p.savefig(paper1_figures_path("{}_moments.png").format(name))
-    p.savefig(paper1_figures_path("{}_moments.pdf").format(name))
+    p.savefig(paper1_figures_path("{}_spectra.png").format(name))
+    p.savefig(paper1_figures_path("{}_spectra.pdf").format(name))
 
     # raw_input("Next plot?")
     p.close()

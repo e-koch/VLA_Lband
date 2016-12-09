50adf4d44e5e5a498d132369a6a18a6885c2220a
e-koch <koch.eric.w@gmail.com>
2016-12-08 18:27:25 -0700

    Save fits as tables and add Lorentzian fit

--- 14B-088/HI/analysis/co_comparison/total_rotsub_profiles.py
+++ 14B-088/HI/analysis/co_comparison/total_rotsub_profiles.py
@@ -5,11 +5,13 @@ from spectral_cube import SpectralCube
 import os
 import numpy as np
 import matplotlib.pyplot as p
+from pandas import DataFrame
 from astropy.modeling import models, fitting
+from astropy.io import fits
 
 from analysis.paths import (fourteenB_HI_data_path, iram_co21_data_path,
-                            paper1_figures_path)
-from analysis.constants import rotsub_cube_name
+                            paper1_figures_path, paper1_tables_path)
+from analysis.constants import rotsub_cube_name, rotsub_mask_name
 from analysis.galaxy_params import gal
 
 '''
@@ -17,11 +19,17 @@ Create profiles of HI and CO after rotation subtraction.
 '''
 
 co_cube = SpectralCube.read(iram_co21_data_path("m33.co21_iram.rotsub.fits"))
+# mask = fits.getdata(iram_co21_data_path("m33.co21_new_assign_clfind.fits"))
+# co_cube = co_cube.with_mask(mask.astype(bool))
 
 hi_cube = SpectralCube.read(fourteenB_HI_data_path(rotsub_cube_name))
+hi_mask = fits.open(fourteenB_HI_data_path(rotsub_mask_name))[0]
+hi_cube = hi_cube.with_mask(hi_mask.data > 0)
 
-total_spectrum_hi = np.empty((hi_cube.shape[0]))
-total_spectrum_co = np.empty((co_cube.shape[0]))
+radius = gal.radius(header=hi_cube.header)
+
+total_spectrum_hi = np.zeros((hi_cube.shape[0]))
+total_spectrum_co = np.zeros((co_cube.shape[0]))
 
 # Create the HI spectrum
 for chan in ProgressBar(range(hi_cube.shape[0])):
@@ -57,6 +65,7 @@ p.plot(hi_cube.spectral_axis.to(u.km / u.s).value, total_spectrum_hi.value,
 p.xlabel("Velocity (km/s)")
 p.ylabel("Total Intensity (Jy)")
 p.xlim([-100, 100])
+p.grid()
 p.draw()
 
 p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_hi.pdf"))
@@ -77,6 +86,7 @@ p.xlabel("Velocity (km/s)")
 p.ylabel("Normalized Total Intensity")
 p.ylim([-0.02, 1.1])
 p.xlim([-100, 100])
+p.grid()
 p.legend()
 p.draw()
 
@@ -151,6 +161,58 @@ p.draw()
 p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_hi_fit.pdf"))
 p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_hi_fit.png"))
 # raw_input("Next plot?")
+p.clf()
+
+# Save parameter table
+hi_param_df = DataFrame({"Params": parvals,
+                         "Errors": [np.sqrt(cov[i, i]) for i in range(cov.shape[0])]},
+                         index=parnames)
+hi_param_df.to_latex(paper1_tables_path("hi_gaussian_totalprof_fits.tex"))
+hi_param_df.to_csv(fourteenB_HI_data_path("tables/hi_gaussian_totalprof_fits.csv",
+                                          no_check=True))
+
+# Lorentzian model
+
+g_HI_init_lor = models.Lorentz1D(amplitude=1., x_0=0., fwhm=5.)
+
+fit_g_lor = fitting.LevMarLSQFitter()
+
+g_HI_lor = fit_g_lor(g_HI_init_lor, vels, norm_intens)
+
+cov = fit_g.fit_info['param_cov']
+parnames = [n for n in g_HI_lor.param_names]
+parvals = [v for (n, v) in zip(g_HI_lor.param_names, g_HI_lor.parameters)
+           if n in parnames]
+print("HI Fit")
+for i, (name, value) in enumerate(zip(parnames, parvals)):
+    print('{}: {} +/- {}'.format(name, value, np.sqrt(cov[i][i])))
+
+# Note that the statistical errors on the mean are too small.
+# Due to my rolling approximation, the error is ~1 channel width.
+
+p.plot(vels, norm_intens, 'b-', drawstyle='steps-mid')
+p.plot(vels, g_HI_lor(vels), 'k:', label="Total Fit")
+p.xlabel("Velocity (km/s)")
+p.ylabel("Total Normalized Intensity")
+p.xlim([-100, 100])
+p.legend()
+p.ylim([-0.1, 1.1])
+p.grid()
+p.draw()
+
+p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_hi_fit_lorentz.pdf"))
+p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_hi_fit_lorentz.png"))
+# raw_input("Next plot?")
+p.clf()
+
+# Save parameter table
+hi_param_df = DataFrame({"Params": g_HI_lor.parameters,
+                         "Errors": [np.sqrt(cov[i, i]) for i in range(cov.shape[0])]},
+                         index=g_HI_lor.param_names)
+hi_param_df.to_latex(paper1_tables_path("hi_gaussian_totalprof_fits_lorentz.tex"))
+hi_param_df.to_csv(fourteenB_HI_data_path("tables/hi_gaussian_totalprof_fits_lorentz.csv",
+                                          no_check=True))
+
 
 # CO model
 
@@ -159,11 +221,12 @@ g_CO_init = models.Gaussian1D(amplitude=1., mean=0., stddev=9.)
 fit_g_co = fitting.LevMarLSQFitter()
 
 co_vels = co_cube.spectral_axis.to(u.km / u.s).value
-norm_co_intens = np.roll((total_spectrum_co / total_spectrum_co.max()).value,
-                         -1)
+norm_co_intens = total_spectrum_co / total_spectrum_co.max()
+# norm_co_intens = np.roll((total_spectrum_co / total_spectrum_co.max()).value,
+#                          -1)
 g_CO = fit_g_co(g_CO_init, co_vels, norm_co_intens)
 
-cov = fit_g.fit_info['param_cov']
+cov = fit_g_co.fit_info['param_cov']
 print("CO(2-1) Fit")
 for i, (name, value) in enumerate(zip(g_CO.param_names, g_CO.parameters)):
     print('{}: {} +/- {}'.format(name, value, np.sqrt(cov[i][i])))
@@ -184,3 +247,11 @@ p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_co21_fit.
 p.savefig(paper1_figures_path("total_profile_corrected_velocity_rotsub_co21_fit.png"))
 
 p.close()
+
+# Save table of parameters
+co_param_df = DataFrame({"Params": g_CO.parameters,
+                         "Errors": [np.sqrt(cov[i, i]) for i in range(cov.shape[0])]},
+                         index=g_CO.param_names)
+co_param_df.to_latex(paper1_tables_path("co_gaussian_totalprof_fits.tex"))
+co_param_df.to_csv(iram_co21_data_path("tables/co_gaussian_totalprof_fits.csv",
+                                       no_check=True))

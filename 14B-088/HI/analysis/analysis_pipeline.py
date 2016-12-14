d82adb051e9c385ceeeb92b8860517f43bee8f23
e-koch <koch.eric.w@gmail.com>
2016-12-14 13:48:28 -0700

    Add running diskfit on peak velocities

--- 14B-088/HI/analysis/analysis_pipeline.py
+++ 14B-088/HI/analysis/analysis_pipeline.py
@@ -11,6 +11,8 @@ from subprocess import call
 from analysis.paths import c_hi_analysispath, fourteenB_HI_data_path
 from analysis.constants import moment1_name
 
+os.sys.path.insert(0, c_hi_analysispath(""))
+
 # os.chdir(c_hi_analysispath(""))
 
 #################
@@ -35,25 +37,39 @@ execfile(c_hi_analysispath("HI_make_moments.py"))
 execfile(c_hi_analysispath("rotation_curves/make_moment1_for_rotcurve.py"))
 
 # Now run diskfit.
+
+# First using the centroids
 diskfit_script = c_hi_analysispath("rotation_curves/run_diskfit.py")
 diskfit_params = c_hi_analysispath("rotation_curves/diskfit_params/diskfit_params_nowarp_noradial_noasymm.inp")
 output_path = fourteenB_HI_data_path("", no_check=True)
 
 call("python {0} {1} {2} {3}".format(diskfit_script, diskfit_params,
-                                     output_path, moment1_name), shell=True)
+                                     output_path,
+                                     fourteenB_HI_data_path(moment1_name)),
+     shell=True)
+
+# And then using peak velocity
+diskfit_params = c_hi_analysispath("rotation_curves/diskfit_params/diskfit_params_peakvels_nowarp_noradial_noasymm.inp")
+output_path = fourteenB_HI_data_path("", no_check=True)
+
+call("python {0} {1} {2} {3}".format(diskfit_script, diskfit_params,
+                                     output_path,
+                                     fourteenB_HI_data_path(moment1_name)),
+     shell=True)
+
 
 # Fit the rotation curve and produce a smooth model
 call("python {0} {1}".format(c_hi_analysispath("rotation_curves/vrot_fit.py"),
                              fourteenB_HI_data_path("diskfit_noasymm_noradial_nowarp_output")), shell=True)
 
 # Residual velocity figures
-execfile(c_hi_analysispath("residual_velocity_surface_figure.py"))
+execfile(c_hi_analysispath("rotation_curves/residual_velocity_surface_figure.py"))
 
 # Subtract the rotation curve from the cube and save.
 execfile(c_hi_analysispath("rotation_curves/cube_subtract_rotation.py"))
 
 # Rotation subtracted slab figures
-execfile(c_hi_analysispath("rotsub_channels_figure.py"))
+execfile(c_hi_analysispath("rotation_curves/rotsub_channels_figure.py"))
 
 # Movie! Turned off by default.
 # execfile(c_hi_analysispath("rotsub_channels_movie.py"))
@@ -91,12 +107,15 @@ execfile(c_hi_analysispath("co_comparison/co_subtract_rotation.py"))
 execfile(c_hi_analysispath("co_comparison/cloud_catalog.py"))
 
 # Radial profiles w/ CO
-execfile(c_hi_analysispath("co_comparison/co_radial_profiles.py"))
+execfile(c_hi_analysispath("co_comparison/co_radial_profile.py"))
 
-execfile(c_hi_analysispath("co_comparison/co_veldisp_profiles.py"))
+execfile(c_hi_analysispath("co_comparison/co_veldisp_profile.py"))
 
 # Adaptive thresholding mask comparison
 execfile(c_hi_analysispath("co_comparison/co_vs_hi_boundaries.py"))
 
 # Rotation subtracted total spectra
 execfile(c_hi_analysispath("co_comparison/total_rotsub_profiles.py"))
+
+# HI and CO spectra at the GMC locations
+execfile(c_hi_analysispath("co_comparison/co_hi_cloud_spectra.py"))

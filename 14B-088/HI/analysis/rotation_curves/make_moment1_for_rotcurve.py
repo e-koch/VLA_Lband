133ffa2a2d5a756df23aecc46a05b97c23db55ba
e-koch <koch.eric.w@gmail.com>
2016-12-15 10:19:49 -0700

    Added Gauss-Hermite fitting to estimate peak velocities

--- 14B-088/HI/analysis/rotation_curves/make_moment1_for_rotcurve.py
+++ 14B-088/HI/analysis/rotation_curves/make_moment1_for_rotcurve.py
@@ -9,7 +9,7 @@ from astropy.io import fits
 from scipy.signal import medfilt
 from multiprocessing import Pool
 from itertools import izip
-
+from lmfit import minimize, Parameters
 
 from analysis.paths import fourteenB_HI_data_path
 from analysis.constants import cube_name, mask_name, pb_lim
@@ -83,14 +83,97 @@ posns = np.where(subcube.mask.include().sum(0) > 0)
 pool = Pool(6)
 output = pool.map(peak_velocity, izip(*posns))
 
+pool.close()
+pool.join()
+
 for out in output:
     peakvels[out[1], out[2]] = out[0]
 
 peakvels[peakvels == 0.0 * u.m / u.s] = np.NaN * u.m / u.s
 
-peak_intens_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5.ellip_mask.peakvels.fits"
+peak_intens_name = "M33_14B-088_HI.clean.image.pbcov_gt_{}.ellip_mask.peakvels.fits".formt(pb_lim)
 peakvels = peakvels.astype(np.float32)
 # peakvels = spectral_peakintensity(subcube).astype(np.float32)
 peakvels.header["BITPIX"] = -32
 peakvels.write(fourteenB_HI_data_path(peak_intens_name, no_check=True),
                overwrite=True)
+
+
+# Trying Gauss-Hermite fits. The basic outline of the lmfit code is from:
+# http://research.endlessfernweh.com/curve-fitting/
+
+gauss_herm_center = Projection(np.zeros(subcube.shape[1:]),
+                               wcs=cube.wcs.celestial,
+                               unit=cube.spectral_axis.unit)
+
+
+def gaussfunc_gh(paramsin, x):
+
+    try:
+        amp = paramsin['amp'].value
+        center = paramsin['center'].value
+        sig = paramsin['sig'].value
+        skew = paramsin['skew'].value
+        kurt = paramsin['kurt'].value
+    except:
+        amp, center, sig, skew, kurt = paramsin
+
+    c1 = -np.sqrt(3)
+    c2 = -np.sqrt(6)
+    c3 = 2 / np.sqrt(3)
+    c4 = np.sqrt(6) / 3.
+    c5 = np.sqrt(6) / 4.
+
+    term1 = (x - center) / sig
+
+    gaustot_gh = amp * np.exp(-.5 * term1 ** 2) * \
+        (1 + skew * (c1 * term1 + c3 * term1 ** 3) +
+         kurt * (c5 + c2 * term1**2 + c4 * term1**4))
+    return gaustot_gh
+
+
+def herm_gauss_fitting(posn):
+    y, x = posn
+    spec = subcube[:, y, x].value
+    spec_axis = subcube.spectral_axis.value
+    chanwidth = np.abs(spec_axis[1] - spec_axis[0])
+
+    p_gh = Parameters()
+    p_gh.add('amp', value=spec.max(), vary=True)
+    p_gh.add('center', value=spec_axis[spec.argmax()], min=np.min(spec_axis),
+             max=np.max(spec_axis))
+    p_gh.add('sig', value=30 * chanwidth, min=chanwidth, max=None)
+    p_gh.add('skew', value=0, vary=True, min=None, max=None)
+    p_gh.add('kurt', value=0, vary=True, min=None, max=None)
+
+    def gausserr_gh(p, x, y):
+        return gaussfunc_gh(p, x) - y
+
+    fitout_gh = minimize(gausserr_gh, p_gh, args=(spec_axis, spec))
+
+    fit_gh = gaussfunc_gh(fitout_gh.params, spec_axis)
+
+    verbose = False
+    if verbose:
+        import matplotlib.pyplot as p
+        p.plot(spec_axis, fit_gh)
+
+    return spec_axis[fit_gh.argmax()], y, x
+
+
+pool = Pool(6)
+output_hg = pool.map(herm_gauss_fitting, izip(*posns))
+
+pool.close()
+pool.join()
+
+for out in output_hg:
+    gauss_herm_center[out[1], out[2]] = out[0] * u.m / u.s
+
+gauss_herm_center[gauss_herm_center == 0.0 * u.m / u.s] = np.NaN * u.m / u.s
+
+gauss_herm_name = "M33_14B-088_HI.clean.image.pbcov_gt_{}.ellip_mask.gh_peak.fits".format(pb_lim)
+gauss_herm_center = gauss_herm_center.astype(np.float32)
+gauss_herm_center.header["BITPIX"] = -32
+gauss_herm_center.write(fourteenB_HI_data_path(gauss_herm_name, no_check=True),
+                        overwrite=True)

e1c148fb603e1de03f51b6ae5a5605f568924cbe
e-koch <koch.eric.w@gmail.com>
2017-02-01 16:56:40 -0700

    Add paths for my cirrus instance

--- paths.py
+++ paths.py
@@ -28,9 +28,9 @@ elif socket.gethostname() == 'caterwauler':
 elif "nmpost" in socket.gethostname():
     root = os.path.expanduser("~/VLA_Lband")
     data_path = os.path.expanduser("~/data")
-elif "segfault" in socket.gethostname():
-    root = os.path.expanduser("'~/Dropbox/code_development/VLA_Lband/")
-    data_path = "/mnt/bigdata/"
+elif "segfault" == socket.gethostname():
+    root = os.path.expanduser("~/Dropbox/code_development/VLA_Lband/")
+    data_path = "/mnt/bigdata/reduction"
 # else:
 #     root = os.path.expanduser('~/work/w51/alma/')
 

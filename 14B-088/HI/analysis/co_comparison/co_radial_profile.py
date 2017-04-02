3121d07815be80830c077d3dabf646d5a3187166
e-koch <koch.eric.w@gmail.com>
2017-04-02 17:01:49 -0600

    Save the radial profiles; moved the Krumholz model comparisons out

--- 14B-088/HI/analysis/co_comparison/co_radial_profile.py
+++ 14B-088/HI/analysis/co_comparison/co_radial_profile.py
@@ -13,12 +13,9 @@ from astropy.table import Table
 from HI_radial_profile import surfdens_radial_profile
 from paths import (iram_co21_data_path, fourteenB_HI_data_path,
                    paper1_figures_path, c_hi_analysispath)
-from constants import moment0_name, cube_name, mask_name
+from constants import moment0_name, cube_name, mask_name, co21_mass_conversion
 from galaxy_params import gal
 from plotting_styles import onecolumn_figure, default_figure
-
-from co_comparison.krumholz_2009_model import (krumholz_ratio_model,
-                                               optimize_clump_factors)
 
 '''
 Create the surface density profile in CO(2-1), assuming a factor to convert
@@ -26,8 +23,6 @@ to the H2 mass.
 '''
 
 default_figure()
-
-co21_mass_conversion = 6.7 * (u.M_sun / u.pc ** 2) / (u.K * u.km / u.s)
 
 # IRAM beam efficiency
 beam_eff = 0.75
@@ -39,7 +34,6 @@ dr = 100 * u.pc
 
 # Load the moment 0
 cube = SpectralCube.read(iram_co21_data_path("m33.co21_iram.fits"))
-del cube._header[""]
 # cube = cube.with_mask(cube > 0.1 * u.K)
 
 mom0_hi = fits.open(fourteenB_HI_data_path(moment0_name))[0]
@@ -153,133 +147,12 @@ p.close()
 
 # p.show()
 
-# Now plot their ratio against the total gas surface density
-gas_ratio = sd.value / sd_hi.value
-gas_ratio_sigma = (gas_ratio * \
-        np.sqrt((sd_sigma / sd)**2 + (sd_sigma_hi / sd_hi)**2)).value
-log_gas_ratio_sigma = gas_ratio_sigma / (gas_ratio * np.log(10))
-total_sd = sd.value + sd_hi.value
-total_sd_sigma = (total_sd * \
-    np.sqrt((sd_sigma / sd)**2 + (sd_sigma_hi / sd_hi)**2)).value
-
-# Overplot the Krumholz model with a few different clumping factors.
-# Theoretically, c -> 1 at a resolution of 100 pc. but I'm finding a better
-# match when c=4-6. The model is supposed to take metallicity into account,
-# but maybe the gradient is causing some issues? Schruba+11 finds c~2 for their
-# entire sample, with a lot of scatter
-sds = np.arange(1, 40, 0.2)
-
-onecolumn_figure(font_scale=1.0, fig_ratio=1.0)
-# p.semilogy(total_sd, gas_ratio, 'bD')
-p.errorbar(total_sd, np.log10(gas_ratio), yerr=log_gas_ratio_sigma,
-           xerr=total_sd_sigma, color='b', alpha=0.6, fmt='D')
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=1, Z=0.5)), "r--",
-       label="c=1, Z=0.5")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=0.5)), "g-.",
-       label="c=4, Z=0.5")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=0.25)), "m.",
-       label="c=4, Z=0.25")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=1.0)), "k-",
-       label="c=4, Z=1.0")
-p.xlabel("$\Sigma_{\mathrm{Gas}}$ (M$_{\odot}$ pc$^{-2}$)")
-p.ylabel("log H$_2$-to-HI Ratio $\Sigma_{\mathrm{H2}} / \Sigma_{\mathrm{HI}}$")
-p.xlim([2, 22])
-p.ylim([-4, 1])
-p.legend(loc='lower right', frameon=True)
-p.grid()
-p.tight_layout()
-
-p.savefig(paper1_figures_path("ratio_totalsigma_w_krumholzmodel_dr_{}pc.pdf".format(int(dr.value))))
-p.savefig(paper1_figures_path("ratio_totalsigma_w_krumholzmodel_dr_{}pc.png".format(int(dr.value))))
-p.close()
-
-# Gratier+16 find evidence for a dark CO component, at about ~5 Msol/pc^2.
-# Let's add this in, assuming the dark component is *only* in the CO and not
-# due to optically thick HI (some portion probably is).
-sd_dark = sd + 5 * u.solMass / u.pc**2
-sd_dark_sigma = (sd_dark * sd_sigma) / sd
-gas_ratio_dark = sd_dark.value / sd_hi.value
-gas_ratio_dark_sigma = (gas_ratio_dark *
-        np.sqrt((sd_dark_sigma / sd_dark)**2 + (sd_sigma_hi / sd_hi)**2)).value
-log_gas_ratio_dark_sigma = gas_ratio_dark_sigma / (gas_ratio_dark * np.log(10))
-
-total_sd_plus_dark = sd_dark.value + sd_hi.value
-total_sd_plus_dark_sigma = (total_sd_plus_dark *
-    np.sqrt((sd_dark_sigma / sd_dark)**2 + (sd_sigma_hi / sd_hi)**2)).value
-
-# p.semilogy(total_sd, gas_ratio, 'bD', label="H$_2$ + HI")
-# p.semilogy(total_sd + 5, gas_ratio_dark, 'ro', label="H$_2$ + HI + Dark H$_2$")
-onecolumn_figure(font_scale=1.0, fig_ratio=1.0)
-
-p.errorbar(total_sd_plus_dark, np.log10(gas_ratio_dark),
-           yerr=log_gas_ratio_dark_sigma,
-           xerr=total_sd_plus_dark_sigma, color='r', alpha=0.6, marker='o',
-           label=r"H$_2$ + HI + CO-dark H$_2$")
-p.errorbar(total_sd, np.log10(gas_ratio), yerr=log_gas_ratio_sigma,
-           xerr=total_sd_sigma, color='b', alpha=0.6, fmt='D',
-           label=r"H$_2$ + HI")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=1, Z=0.5)), "r--",
-       label="c=1, Z=0.5")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=0.5)), "g-.",
-       label="c=4, Z=0.5")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=0.25)), "m.",
-       label="c=4, Z=0.25")
-p.plot(sds, np.log10(krumholz_ratio_model(sds, c=4, Z=1.0)), "k-",
-       label="c=4, Z=1.0")
-p.xlabel("$\Sigma_{\mathrm{Gas}}$ (M$_{\odot}$ pc$^{-2}$)")
-p.ylabel("log H$_2$-to-HI Ratio $\Sigma_{\mathrm{H2}} / \Sigma_{\mathrm{HI}}$")
-p.xlim([2, 25])
-p.ylim([-4, 1])
-p.legend(loc='lower right', frameon=True)
-p.grid()
-p.tight_layout()
-
-p.savefig(paper1_figures_path("ratio_totalsigma_dark_w_krumholzmodel_dr_{}pc.pdf".format(int(dr.value))))
-p.savefig(paper1_figures_path("ratio_totalsigma_dark_w_krumholzmodel_dr_{}pc.png".format(int(dr.value))))
-p.close()
-
-# But M33 has a known metallicity gradient, so we can do a bit better
-# Clumping factors should converge to 1 on 100 pc, based on the Krumholz model
-# This isn't happening here, so let's what c needs to be for the curve to
-# intersect with each point we have.
-
-# Metallicity of 0.5
-clump_constz = optimize_clump_factors(total_sd, gas_ratio, Z=0.5)
-
-
-# Metallicity Gradient from Roso & Simon (2005)
-def ros_sim_metallicity(radius):
-    return 10 ** (8.36 - 0.027 * radius - 8.8)
-
-
-clump_rossim = optimize_clump_factors(total_sd, gas_ratio,
-                                      Z=ros_sim_metallicity(rs.value))
-
-
-# And from Bresolin 2011
-def bresolin_metallicity(radius):
-    return 10 ** (8.82 - 0.03 * radius - 8.8)
-
-
-clump_bresolin = optimize_clump_factors(total_sd, gas_ratio,
-                                        Z=bresolin_metallicity(rs.value))
-
-onecolumn_figure(font_scale=1.0)
-
-p.plot(rs.value[:-1], clump_constz[:-1], 'bD-', label="Z=0.5")
-p.plot(rs.value[:-1], clump_rossim[:-1], 'ro--',
-       label="Rosolowsky & Simon (2005)")
-p.plot(rs.value[:-1], clump_bresolin[:-1], 'gs-.', label="Bresolin (2011)")
-p.legend(loc='best', frameon=True)
-p.ylim([-1, 10])
-p.grid()
-p.ylabel("Clumping Factor")
-p.xlabel("Radius (kpc)")
-p.tight_layout()
-
-p.savefig(paper1_figures_path("clumpfactor_krumholzmodel_dr_{}pc.pdf".format(int(dr.value))))
-p.savefig(paper1_figures_path("clumpfactor_krumholzmodel_dr_{}pc.png".format(int(dr.value))))
-p.close()
+# Save the radial profiles in a FITS table
+table = Table([rs, sd, sd_sigma, sd_hi, sd_sigma_hi],
+              names=('Radius', "CO_Sigma", "CO_Sigma_std", "HI_Sigma",
+                     "HI_Sigma_std"))
+table.write(fourteenB_HI_data_path("tables/co21_hi_radialprofiles_{}pc.fits".format(int(dr.value)),
+                                   no_check=True))
 
 # Also plot the total gas surface density against the stellar surface density
 # from Corbelli
@@ -320,11 +193,7 @@ rs_hi_m, sd_hi_m, sd_sigma_hi_m = \
                             max_rad=6 * u.kpc, dr=dr,
                             weight_type='mass',
                             beam=average_beams(hi_cube.beams))
-# Apply scaling factor
-# sd_hi_m /= 1.45
-# sd_sigma_hi_m /= 1.45
 
-#
 p.errorbar(np.log10(sd.value), np.log10(sd_m.value),
            xerr=0.434 * sd_sigma.value / sd.value,
            yerr=0.434 * sd_sigma_m.value / sd_m.value,

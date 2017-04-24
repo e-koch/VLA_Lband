b68563cb727c03b29ac3b3b4dc93151a71a64814
e-koch <koch.eric.w@gmail.com>
2017-04-24 14:09:33 -0600

    Compare the avg line width to the stacked widths

--- 14B-088/HI/analysis/HI_veldisp_profile.py
+++ 14B-088/HI/analysis/HI_veldisp_profile.py
@@ -1,12 +1,11 @@
 
 from spectral_cube import SpectralCube
 from spectral_cube.lower_dimensional_structures import Projection
-from radio_beam import Beam
 import numpy as np
 import astropy.units as u
 from astropy.coordinates import Angle
 from astropy.io import fits
-from astropy.wcs import WCS
+from astropy.utils.console import ProgressBar
 
 
 def radial_profile(gal, moment, header=None, dr=100 * u.pc,
@@ -102,30 +101,58 @@ def radial_profile(gal, moment, header=None, dr=100 * u.pc,
     return radprof, sdprof, sdprof_sigma
 
 
+def total_profile(cube, spatial_mask=None, verbose=True):
+    '''
+    Create the total profile over a region in a given spatial mask.
+    '''
+
+    posns = np.where(spatial_mask)
+
+    if verbose:
+        spec_iter = ProgressBar(posns[0].size)
+    else:
+        spec_iter = xrange(posns[0].size)
+
+    total_profile = np.zeros((cube.shape[0],)) * cube.unit
+
+    for i in spec_iter:
+        y, x = posns[0][i], posns[1][i]
+
+        spec = cube[:, y, x]
+        mask_spec = cube.mask.include(view=(slice(None), y, x))
+        valid = np.logical_and(np.isfinite(spec), mask_spec)
+
+        total_profile[valid] += spec[valid]
+
+    return total_profile
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as p
+    from spectral_cube.cube_utils import average_beams
+    from astropy.modeling import models, fitting
+    from pandas import DataFrame
 
     from paths import (fourteenB_HI_data_path, arecibo_HI_data_path,
                        c_hi_analysispath, paper1_figures_path,
-                       data_path)
+                       data_path, paper1_tables_path)
 
-    from constants import moment0_name, lwidth_name
+    from constants import (lwidth_name, rotsub_cube_name,
+                           rotsub_mask_name, hi_freq,
+                           centroidsub_cube_name, centroidsub_mask_name,
+                           peakvelsub_cube_name, peakvelssub_mask_name)
+
     from galaxy_params import gal
     from plotting_styles import default_figure, onecolumn_figure
 
 
     lwidth_hdu = fits.open(fourteenB_HI_data_path(lwidth_name))[0]
-    lwidth = Projection(lwidth_hdu.data, wcs=WCS(lwidth_hdu.header),
-                        unit=u.m / u.s)
-    lwidth.meta["beam"] = Beam.from_fits_header(lwidth_hdu.header)
+    lwidth = Projection.from_hdu(lwidth_hdu).to(u.km / u.s)
 
     # Create a radial profile of the HI vel disp out to 8 kpc.
     # Beyond 8 kpc, noise is dominant. It may be some reflection of the
     # warp, but I don't trust it right now.
     rs, sd, sd_sigma = radial_profile(gal, lwidth, max_rad=8 * u.kpc)
-
-    sd = sd.to(u.km / u.s)
-    sd_sigma = sd_sigma.to(u.km / u.s)
 
     onecolumn_figure(font_scale=1.)
     p.errorbar(rs.value, sd.value,
@@ -179,29 +206,144 @@ if __name__ == "__main__":
     # raw_input("Next plot?")
     p.close()
 
-    # There are interesting drops at 1 and ~4.2 kpc. Plot these on the moment 0
-    # mom0 = fits.getdata(fourteenB_HI_data_path(moment0_name))
+    # Now do line stacking with the same bin size
 
-    # ax = p.subplot(121, projection=lwidth.wcs)
-    # p.imshow(mom0, origin='lower')
-    # radii = gal.radius(header=lwidth.header)
-    # p.contour(radii <= 1 * u.kpc, colors='b')
-    # p.contour(radii <= 4.2 * u.kpc, colors='g')
-    # p.xlabel("")
-    # ax.set_title("Zeroth Moment")
-    # ax2 = p.subplot(122, projection=lwidth.wcs)
-    # p.imshow(lwidth.value, origin='lower')
-    # p.contour(radii <= 1 * u.kpc, colors='b')
-    # p.contour(radii <= 4.2 * u.kpc, colors='g')
-    # p.xlabel("")
-    # ax2.set_title("Line Width")
-    # lat = ax2.coords[1]
-    # lat.set_ticklabel_visible(False)
-    # p.draw()
+    hi_cube = SpectralCube.read(fourteenB_HI_data_path(rotsub_cube_name))
+    hi_mask = fits.open(fourteenB_HI_data_path(rotsub_mask_name))[0]
+    hi_cube = hi_cube.with_mask(hi_mask.data > 0)
 
-    # p.savefig(paper1_figures_path("moment0_w_veldisp_minima.png"))
-    # p.savefig(paper1_figures_path("moment0_w_veldisp_minima.pdf"))
+    hi_cube_cent = SpectralCube.read(fourteenB_HI_data_path(centroidsub_cube_name))
+    hi_mask_cent = fits.open(fourteenB_HI_data_path(centroidsub_mask_name))[0]
+    hi_cube_cent = hi_cube_cent.with_mask(hi_mask_cent.data > 0)
 
-    # p.close()
+    hi_cube_peakvel = SpectralCube.read(fourteenB_HI_data_path(peakvelsub_cube_name))
+    hi_mask_peakvel = fits.open(fourteenB_HI_data_path(peakvelssub_mask_name))[0]
+    hi_cube_peakvel = hi_cube_cent.with_mask(hi_mask_peakvel.data > 0)
+
+    hi_beam = average_beams(hi_cube.beams)
+
+    hi_radius = gal.radius(header=hi_cube.header)
+
+    dr = 100 * u.pc
+    max_radius = (8.0 * u.kpc).to(u.pc)
+
+    nbins = np.int(np.floor(max_radius / dr))
+    inneredge = np.linspace(0, max_radius - dr, nbins)
+    outeredge = np.linspace(dr, max_radius, nbins)
+
+    total_spectrum_hi_radial = \
+        np.zeros((inneredge.size, hi_cube.shape[0])) * u.K
+    total_spectrum_hi_radial_cent = \
+        np.zeros((inneredge.size, hi_cube.shape[0])) * u.K
+    total_spectrum_hi_radial_peakvel = \
+        np.zeros((inneredge.size, hi_cube.shape[0])) * u.K
+
+    for ctr, (r0, r1) in enumerate(zip(inneredge,
+                                       outeredge)):
+
+        print("On bin {} to {}".format(r0.value, r1))
+
+        hi_rad_mask = np.logical_and(hi_radius >= r0,
+                                     hi_radius < r1)
+
+        total_spectrum_hi_radial[ctr] = \
+            total_profile(hi_cube, hi_rad_mask).to(u.K, equivalencies=hi_beam.jtok_equiv(hi_freq))
+
+        total_spectrum_hi_radial_cent[ctr] = \
+            total_profile(hi_cube_cent, hi_rad_mask).to(u.K, equivalencies=hi_beam.jtok_equiv(hi_freq))
+
+        total_spectrum_hi_radial_peakvel[ctr] = \
+            total_profile(hi_cube_peakvel, hi_rad_mask).to(u.K, equivalencies=hi_beam.jtok_equiv(hi_freq))
+
+    g_HI_init = models.Gaussian1D(amplitude=1., mean=0., stddev=10.)
+
+    hi_params = {}
+    labels = ["rotsub", "centsub", "peaksub"]
+
+    for sub in labels:
+        for name in g_HI_init.param_names:
+            par_name = "{0}_{1}".format(sub, name)
+            par_error = "{}_stderr".format(par_name)
+
+            hi_params[par_name] = np.zeros_like(inneredge.value)
+            hi_params[par_error] = np.zeros_like(inneredge.value)
+
+    for ctr, (r0, r1) in enumerate(zip(inneredge,
+                                       outeredge)):
+
+        hi_spectra = [total_spectrum_hi_radial[ctr],
+                      total_spectrum_hi_radial_cent[ctr],
+                      total_spectrum_hi_radial_peakvel[ctr]]
+
+        for spectrum, label in zip(hi_spectra, labels):
+
+            fit_g = fitting.LevMarLSQFitter()
+
+            vels = hi_cube.spectral_axis.to(u.km / u.s).value
+            norm_intens = (spectrum / spectrum.max()).value
+            g_HI = fit_g(g_HI_init, vels, norm_intens, maxiter=1000)
+
+            cov = fit_g.fit_info['param_cov']
+            if cov is None:
+                raise Exception("No covariance matrix")
+
+            idx_corr = 0
+            for idx, name in enumerate(g_HI.param_names):
+                if name == "mean_1":
+                    idx_corr = 1
+                    continue
+                par_name = "{0}_{1}".format(label, name)
+                hi_params[par_name][ctr] = g_HI.parameters[idx]
+                hi_params["{}_stderr".format(par_name)][ctr] = \
+                    np.sqrt(cov[idx - idx_corr, idx - idx_corr])
+
+    bin_names = ["{}-{}".format(r0.value, r1)
+                 for r0, r1 in zip(inneredge, outeredge)]
+
+    bin_center = (inneredge + dr / 2.).to(u.kpc)
+    hi_params["bin_center"] = bin_center
+
+    hi_radial_fits = DataFrame(hi_params, index=bin_names)
+
+    bin_string = "{0}{1}".format(int(dr.value), dr.unit)
+    hi_radial_fits.to_latex(paper1_tables_path("hi_gaussian_totalprof_fits_radial_{}.tex".format(bin_string)))
+    hi_radial_fits.to_csv(fourteenB_HI_data_path("tables/hi_gaussian_totalprof_fits_radial_{}.csv".format(bin_string),
+                                                 no_check=True))
+
+    hi_velres = \
+        (hi_cube.spectral_axis[1] -
+         hi_cube.spectral_axis[0]).to(u.km / u.s).value
+
+    # Add the velocity width of the channel in quadrature
+    hi_width_error = lambda val: np.sqrt(val**2 + hi_velres**2)
+
+    onecolumn_figure(font_scale=1.)
+    p.errorbar(rs.value, sd.value,
+               yerr=sd_sigma.value, fmt="-",
+               drawstyle='steps-mid', label='Averaged Line Width')
+    p.xlabel("Radius (kpc)")
+    p.ylabel("HI Velocity Dispersion (km/s)")
+
+    # Now the stacked fits
+    p.errorbar(hi_radial_fits['bin_center'],
+               hi_radial_fits['rotsub_stddev'],
+               yerr=hi_width_error(hi_radial_fits['rotsub_stddev_stderr']),
+               fmt='D', label='Rotation Stacked Fit', alpha=0.5)
+    p.errorbar(hi_radial_fits['bin_center'],
+               hi_radial_fits['centsub_stddev'],
+               yerr=hi_width_error(hi_radial_fits['centsub_stddev_stderr']),
+               fmt='o', label='Centroid Stacked Fit', alpha=0.5)
+    p.errorbar(hi_radial_fits['bin_center'],
+               hi_radial_fits['peaksub_stddev'],
+               yerr=hi_width_error(hi_radial_fits['peaksub_stddev_stderr']),
+               fmt='^', label='Peak Stacked Fit', alpha=0.5)
+    p.grid()
+    p.legend(frameon=True)
+    p.tight_layout()
+
+    p.savefig(paper1_figures_path("hi_veldisp_w_stackedfits.png"))
+    p.savefig(paper1_figures_path("hi_veldisp_w_stackedfits.pdf"))
+
+    p.close()
 
     default_figure()

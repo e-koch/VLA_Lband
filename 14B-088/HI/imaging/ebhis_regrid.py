0aec312748dd657cae7e033c9045cc024b636896
e-koch <koch.eric.w@gmail.com>
2017-05-09 18:28:28 -0600

    Update the SD regridding scripts

--- 14B-088/HI/imaging/ebhis_regrid.py
+++ 14B-088/HI/imaging/ebhis_regrid.py
@@ -6,8 +6,9 @@ Regrid the EBHIS data to match the VLA HI data.
 from spectral_cube import SpectralCube
 from astropy.utils.console import ProgressBar
 import numpy as np
-import astropy.io.fits as fits
 import os
+
+from cube_analysis.io_utils import create_huge_fits
 
 from paths import fourteenB_HI_data_path, data_path
 
@@ -17,21 +18,26 @@ vla_cube = SpectralCube.read(fourteenB_HI_data_path("M33_14B-088_HI.clean.image.
 
 ebhis_path = os.path.join(data_path, "EBHIS")
 
-cube = SpectralCube.read(os.path.join(ebhis_path, "CAR_C02.fit"))
-
-# Cut to the spatial extents, since this chunk of EBHIS covers so much more
-cube = cube.subcube(xlo=vla_cube.longitude_extrema[1],
-                    xhi=vla_cube.longitude_extrema[0],
-                    ylo=vla_cube.latitude_extrema[0],
-                    yhi=vla_cube.latitude_extrema[1])
-
 # Spectral interpolation, followed by reprojection.
+save_name = os.path.join(ebhis_path, "14B-088_items/m33_ebhis_14B088_spectralregrid_test.fits")
 
-cube = cube.spectral_interpolate(vla_cube.spectral_axis)
+# Only load in the original data if the spectrally-interpolated cube hasn't
+# been saved
+if not os.path.exists(save_name):
 
-# Write out the spectrally interpolated cube
-save_name = os.path.join(ebhis_path, "14B-088_items/m33_ebhis_14B088_spectralregrid.fits")
-cube.write(save_name)
+    cube = SpectralCube.read(os.path.join(ebhis_path, "CAR_C02.fit"))
+
+    # Cut to the spatial extents, since this chunk of EBHIS covers so much more
+    cube = cube.subcube(xlo=vla_cube.longitude_extrema[1],
+                        xhi=vla_cube.longitude_extrema[0],
+                        ylo=vla_cube.latitude_extrema[0],
+                        yhi=vla_cube.latitude_extrema[1])
+
+    cube = cube.spectral_interpolate(vla_cube.spectral_axis)
+    # Write out the spectrally interpolated cube
+    cube.write(save_name)
+else:
+    cube = SpectralCube.read(save_name)
 
 # Make the reprojected header
 new_header = cube.header.copy()
@@ -54,19 +60,12 @@ new_header["BITPIX"] = -32
 
 # Build up the reprojected cube per channel
 save_name = os.path.join(ebhis_path, "14B-088_items/m33_ebhis_14B088.fits")
-output_fits = fits.StreamingHDU(save_name, new_header)
-
-fill_plane = np.zeros_like(vla_cube[0].value, dtype=np.float32) * np.NaN
-for chan in ProgressBar(cube.shape[0]):
-    output_fits.write(fill_plane)
-output_fits.close()
-
-output_fits = fits.open(save_name, mode='update')
+output_fits = create_huge_fits(save_name, new_header, return_hdu=True)
 
 targ_header = vla_cube[0].header
 for chan in ProgressBar(cube.shape[0]):
-    output_fits[0].data[chan] = \
-        cube[chan].reproject(targ_header).value.astype(np.float32)
-    if chan % 20 == 0:
+    proj = cube[chan].reproject(targ_header).value.astype(np.float32)
+    output_fits[0].data[chan] = proj
+    if chan % 100 == 0:
         output_fits.flush()
 output_fits.close()
--- 14B-088/HI/imaging/gbt_regrid.py
+++ 14B-088/HI/imaging/gbt_regrid.py
@@ -6,8 +6,9 @@ Regrid the GBT data to match the VLA HI data.
 from spectral_cube import SpectralCube
 from astropy.utils.console import ProgressBar
 import numpy as np
-import astropy.io.fits as fits
 import os
+
+from cube_analysis.io_utils import create_huge_fits
 
 from paths import fourteenB_HI_data_path, data_path
 
@@ -19,13 +20,27 @@ gbt_path = os.path.join(data_path, "GBT")
 
 cube = SpectralCube.read(os.path.join(gbt_path, "m33_gbt_vlsr_highres.fits"))
 
-# Spectral interpolation, followed by reprojection.
-cube = cube.spectral_interpolate(vla_cube.spectral_axis)
+# Ta* to T_mb as per @low-sky
+Tmb_conv = 1.052
+
 save_name = os.path.join(gbt_path, "14B-088_items/m33_gbt_vlsr_highres_Tmb_14B088_spectralregrid.fits")
-# Cut to the VLA map extents. Technically need to reproject, but this by-eye
-# pixel selection is just slightly beyond the VLA data
-# cube[:, 20:80, 40:80].write(save_name)
-cube.write(save_name)
+
+# Spectral interpolation, followed by reprojection.
+if not os.path.exists(save_name):
+
+    cube = cube.spectral_interpolate(vla_cube.spectral_axis)
+
+    if cube._is_huge:
+        output_fits = create_huge_fits(save_name, cube.header, return_hdu=True)
+
+        for chan in ProgressBar(cube.shape[0]):
+            output_fits[0].data[chan] = cube[chan].value * Tmb_conv
+        output_fits.flush()
+        output_fits.close()
+    else:
+        (cube * Tmb_conv).write(save_name, overwrite=True)
+else:
+    cube = SpectralCube.read(save_name)
 
 # Make the reprojected header
 new_header = cube.header.copy()
@@ -50,21 +65,74 @@ new_header.comments['BUNIT'] = 'Tmb'
 
 # Build up the reprojected cube per channel
 save_name = os.path.join(gbt_path, "14B-088_items/m33_gbt_vlsr_highres_Tmb_14B088.fits")
-output_fits = fits.StreamingHDU(save_name, new_header)
-
-fill_plane = np.zeros_like(vla_cube[0].value, dtype=np.float32) * np.NaN
-for chan in ProgressBar(cube.shape[0]):
-    output_fits.write(fill_plane)
-output_fits.close()
-
-output_fits = fits.open(save_name, mode='update')
+output_fits = create_huge_fits(save_name, new_header, return_hdu=True)
 
 targ_header = vla_cube[0].header
 for chan in ProgressBar(cube.shape[0]):
     reproj_chan = \
         cube[chan].reproject(targ_header).value.astype(np.float32)
-    # Ta* to T_mb as per @low-sky
-    output_fits[0].data[chan] = reproj_chan * 1.052
-    if chan % 20 == 0:
+    output_fits[0].data[chan] = reproj_chan
+    if chan % 200 == 0:
+        output_fits.flush()
+output_fits.close()
+
+
+# Now do it again from the native gridding size
+
+cube = SpectralCube.read(os.path.join(gbt_path, "m33_gbt_vlsr.fits"))
+
+# Ta* to T_mb as per @low-sky
+Tmb_conv = 1.052
+
+save_name = os.path.join(gbt_path, "14B-088_items/m33_gbt_vlsr_Tmb_14B088_spectralregrid.fits")
+
+# Spectral interpolation, followed by reprojection.
+if not os.path.exists(save_name):
+
+    cube = cube.spectral_interpolate(vla_cube.spectral_axis)
+
+    if cube._is_huge:
+        output_fits = create_huge_fits(save_name, cube.header, return_hdu=True)
+
+        for chan in ProgressBar(cube.shape[0]):
+            output_fits[0].data[chan] = cube[chan].value * Tmb_conv
+        output_fits.flush()
+        output_fits.close()
+    else:
+        (cube * Tmb_conv).write(save_name, overwrite=True)
+else:
+    cube = SpectralCube.read(save_name)
+
+# Make the reprojected header
+new_header = cube.header.copy()
+new_header["NAXIS"] = 3
+new_header["NAXIS1"] = vla_cube.shape[2]
+new_header["NAXIS2"] = vla_cube.shape[1]
+new_header["NAXIS3"] = vla_cube.shape[0]
+kwarg_skip = ['TELESCOP', 'BUNIT', 'INSTRUME']
+for key in cube.header:
+    if key == 'HISTORY':
+        continue
+    if key in vla_cube.header:
+        if "NAXIS" in key:
+            continue
+        if key in kwarg_skip:
+            continue
+        new_header[key] = vla_cube.header[key]
+new_header.update(cube.beam.to_header_keywords())
+new_header["BITPIX"] = -32
+# We're going to convert to Tmb below
+new_header.comments['BUNIT'] = 'Tmb'
+
+# Build up the reprojected cube per channel
+save_name = os.path.join(gbt_path, "14B-088_items/m33_gbt_vlsr_Tmb_14B088.fits")
+output_fits = create_huge_fits(save_name, new_header, return_hdu=True)
+
+targ_header = vla_cube[0].header
+for chan in ProgressBar(cube.shape[0]):
+    reproj_chan = \
+        cube[chan].reproject(targ_header).value.astype(np.float32)
+    output_fits[0].data[chan] = reproj_chan
+    if chan % 200 == 0:
         output_fits.flush()
 output_fits.close()

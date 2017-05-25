7c8c6fb26dbf22e9fa09b1121683957123d9b903
e-koch <koch.eric.w@gmail.com>
2017-05-25 10:35:34 -0600

    Create a dirty cubes of the whole HI 14B-088 data

---
+++ 14B-088/HI/imaging/HI_dirty_cube.py
@@ -0,0 +1,57 @@
+
+'''
+Create a dirty HI cube for comparison and use in feathering.
+
+*Note*: Ran with casa-prerelease-5.0.0-187.el7 to take advantage of tclean's
+read-only mode, which speeds things up considerably.
+'''
+
+import os
+
+from tasks import tclean, impbcor
+
+# CASA init should have the VLA_Lband repo appended to the path
+from paths import data_path
+
+# The full MS isn't in the same path as the usual data products.
+full_path = os.path.join(data_path, "../VLA/14B-088/Lines/HI/")
+
+output_path = os.path.join(full_path, "dirty_cube")
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+
+# Image ALL channels in the continuum subtracted MS (~2000).
+# Keep the same spatial settings as is used for the cleaned cubes.
+
+tclean(vis=os.path.join(full_path, '14B-088_HI.ms.contsub'),
+       datacolumn='data',
+       imagename=os.path.join(output_path, 'M33_14B-088_HI.dirty'),
+       field='M33*',
+       imsize=[2560, 2560],
+       cell='3arcsec',
+       specmode='cube',
+       start=1,
+       width=1,
+       nchan=2001,
+       startmodel=None,
+       gridder='mosaic',
+       weighting='natural',
+       niter=0,
+       threshold='3.2mJy/beam',
+       phasecenter='J2000 01h33m50.904 +30d39m35.79',
+       restfreq='1420.40575177MHz',
+       outframe='LSRK',
+       pblimit=0.1,
+       usemask='pb',
+       mask=None,
+       deconvolver='hogbom',
+       pbcor=False,
+       chanchunks=-1
+       )
+
+# Apply pb correction
+impbcor(imagename=os.path.join(output_path, 'M33_14B-088_HI.dirty.image'),
+        pbimage=os.path.join(output_path, 'M33_14B-088_HI.dirty.pb'),
+        outfile=os.path.join(output_path, 'M33_14B-088_HI.dirty.image,pbcor'))

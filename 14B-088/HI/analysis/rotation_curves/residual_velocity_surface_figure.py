1699822d2b3e872cb6f8c5eb2f66349273cf049a
e-koch <koch.eric.w@gmail.com>
2017-06-14 15:58:07 -0600

    Update residual velocity plots and add feathered versions

--- 14B-088/HI/analysis/rotation_curves/residual_velocity_surface_figure.py
+++ 14B-088/HI/analysis/rotation_curves/residual_velocity_surface_figure.py
@@ -2,82 +2,121 @@
 import os
 from astropy.io import fits
 from astropy.wcs import WCS
-import matplotlib.pyplot as p
-import numpy as np
+import matplotlib.pyplot as plt
 
-from analysis.paths import fourteenB_HI_data_path, paper1_figures_path
-from analysis.plotting_styles import default_figure, twocolumn_twopanel_figure
-from analysis.constants import moment1_name
+from paths import (fourteenB_HI_data_path, paper1_figures_path,
+                   fourteenB_HI_file_dict, fourteenB_HI_data_wGBT_path,
+                   fourteenB_wGBT_HI_file_dict, allfigs_path)
+from plotting_styles import (default_figure, twocolumn_twopanel_figure,
+                             twocolumn_figure)
 
 '''
 Plot the residual velocities from disk fit, and from subtracting the
 smoothed version.
 '''
 
-direc = fourteenB_HI_data_path("diskfit_noasymm_noradial_nowarp_output/",
+direc = fourteenB_HI_data_path("diskfit_peakvels_noasymm_noradial_nowarp_output/",
                                no_check=True)
 
 res = fits.open(os.path.join(direc, "rad.res.fits"))[0]
 res_fit = fits.open(os.path.join(direc, "rad.fitres.fits"))[0]
 
-ax2 = p.subplot(111, projection=WCS(res_fit.header))
-im2 = ax2.imshow(res.data / 1000., origin='lower', cmap='seismic',
-                 vmax=30, vmin=-30)
-
-cb2 = p.colorbar(im2)
-cb2.set_label("Residual Velocity (km s$^{-1}$)")
-
-p.draw()
-
-p.savefig(paper1_figures_path("M33_residual_velocity_diskfit.pdf"))
-p.savefig(paper1_figures_path("M33_residual_velocity_diskfit.png"))
-
-# raw_input("Next plot?")
-p.clf()
-
-ax2 = p.subplot(111, projection=WCS(res_fit.header))
+default_figure()
+ax2 = plt.subplot(111, projection=WCS(res_fit.header))
 im2 = ax2.imshow(res_fit.data / 1000., origin='lower', cmap='seismic',
                  vmax=30, vmin=-30)
 
-cb2 = p.colorbar(im2)
+cb2 = plt.colorbar(im2)
 cb2.set_label("Residual Velocity (km s$^{-1}$)")
 
-p.draw()
+plt.savefig(allfigs_path("M33_residual_velocity_diskfit_fit.pdf"))
+plt.savefig(allfigs_path("M33_residual_velocity_diskfit_fit.png"))
 
-p.savefig(paper1_figures_path("M33_residual_velocity_diskfit_fit.pdf"))
-p.savefig(paper1_figures_path("M33_residual_velocity_diskfit_fit.png"))
-
-p.close()
+plt.close()
 
 # Make a side-by-side centroid and residual plot.
 
 twocolumn_twopanel_figure(fig_ratio=0.5)
 
-ax2 = p.subplot(122, projection=WCS(res_fit.header))
-im2 = ax2.imshow(res_fit.data / 1000., origin='lower', cmap='seismic',
-                 vmax=30, vmin=-30)
+ax2 = plt.subplot(122, projection=WCS(res_fit.header))
+im2 = ax2.imshow(res_fit.data / 1000., origin='lower', cmap='viridis',
+                 vmax=20, vmin=-20)
 ax2.set_xlabel("RA (J2000)")
 ax2.coords[1].set_ticklabel_visible(False)
 
-cb2 = p.colorbar(im2)
+cb2 = plt.colorbar(im2)
 cb2.set_label("Residual Velocity (km s$^{-1}$)")
 
-moment1 = fits.open(fourteenB_HI_data_path(moment1_name))[0]
+# moment1 = fits.open(fourteenB_HI_file_dict['Moment1'])[0]
+peakvel = fits.open(fourteenB_HI_file_dict['PeakVels'])[0]
 
-ax = p.subplot(121, projection=WCS(moment1.header))
-im = ax.imshow(moment1.data / 1000.,
+ax = plt.subplot(121, projection=WCS(peakvel.header))
+im = ax.imshow(peakvel.data / 1000.,
                origin='lower', vmin=-300, vmax=-70,
                interpolation='nearest', cmap='viridis')
 
 ax.set_ylabel("DEC (J2000)")
 ax.set_xlabel("RA (J2000)")
 
-cbar = p.colorbar(im)
-cbar.set_label(r"Centroid Velocity (km s$^{-1}$)")
+cbar = plt.colorbar(im)
+cbar.set_label(r"Peak Velocity (km s$^{-1}$)")
 
-p.savefig(paper1_figures_path("M33_centroid_residualfit.pdf"))
-p.savefig(paper1_figures_path("M33_centroid_residualfit.png"))
+plt.savefig(allfigs_path("M33_peakvel_residualfit.pdf"))
+plt.savefig(allfigs_path("M33_peakvel_residualfit.png"))
 
-p.close()
+plt.close()
+
+# Now make similar plots with the feathered data
+
+direc = fourteenB_HI_data_wGBT_path("diskfit_peakvels_noasymm_noradial_nowarp_output/",
+                                    no_check=True)
+
+res_feath = fits.open(os.path.join(direc, "rad.res.fits"))[0]
+res_fit_feath = fits.open(os.path.join(direc, "rad.fitres.fits"))[0]
+
+default_figure()
+ax2 = plt.subplot(111, projection=WCS(res_fit.header))
+im2 = ax2.imshow(res_fit_feath.data / 1000., origin='lower', cmap='seismic',
+                 vmax=30, vmin=-30)
+
+cb2 = plt.colorbar(im2)
+cb2.set_label("Residual Velocity (km s$^{-1}$)")
+
+plt.savefig(allfigs_path("M33_residual_velocity_diskfit_fit_feathered.pdf"))
+plt.savefig(allfigs_path("M33_residual_velocity_diskfit_fit_feathered.png"))
+
+plt.close()
+
+# Make a side-by-side centroid and residual plot.
+
+twocolumn_twopanel_figure(fig_ratio=0.5)
+
+ax2 = plt.subplot(122, projection=WCS(res_fit_feath.header))
+im2 = ax2.imshow(res_fit_feath.data / 1000., origin='lower', cmap='viridis',
+                 vmax=20, vmin=-20)
+ax2.set_xlabel("RA (J2000)")
+ax2.coords[1].set_ticklabel_visible(False)
+
+cb2 = plt.colorbar(im2)
+cb2.set_label("Residual Velocity (km s$^{-1}$)")
+
+moment1 = fits.open(fourteenB_wGBT_HI_file_dict['Moment1'])[0]
+peakvel = fits.open(fourteenB_wGBT_HI_file_dict['PeakVels'])[0]
+
+ax = plt.subplot(121, projection=WCS(peakvel.header))
+im = ax.imshow(peakvel.data / 1000.,
+               origin='lower', vmin=-300, vmax=-70,
+               interpolation='nearest', cmap='viridis')
+
+ax.set_ylabel("DEC (J2000)")
+ax.set_xlabel("RA (J2000)")
+
+cbar = plt.colorbar(im)
+cbar.set_label(r"Peak Velocity (km s$^{-1}$)")
+
+plt.savefig(allfigs_path("M33_peakvel_residualfit_feathered.pdf"))
+plt.savefig(allfigs_path("M33_peakvel_residualfit_feathered.png"))
+
+plt.close()
 
 default_figure()

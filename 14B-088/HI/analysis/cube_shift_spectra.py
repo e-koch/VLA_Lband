a0c1c86156cce70541ba0ed8069e1723b3bfa112
e-koch <koch.eric.w@gmail.com>
2017-06-14 16:40:56 -0600

    Add rotation model subtraction using nicer setup; delete old rotsub file; add file to do subtracted feathered cubes

--- 14B-088/HI/analysis/cube_shift_spectra.py
+++ 14B-088/HI/analysis/cube_shift_spectra.py
@@ -2,64 +2,85 @@
 '''
 Given a velocity surface, shift all spectra in the cube.
 
-Makes 1 version shifted wrt. the centroids.
-Makes another shifted wrt. the peak velocity.
+Mask versions shifted w.r.t:
+
+ * centroids
+ * peak velocities
+ * rotation model
 
 '''
 
 from spectral_cube import SpectralCube
 from astropy.io import fits
 import astropy.units as u
+from astropy import log
 from multiprocessing import Pool
 
 from cube_analysis.spectra_shifter import cube_shifter
 
-from paths import fourteenB_HI_data_path
-from constants import (cube_name, centroidsub_cube_name, moment1_name,
-                       mask_name, centroidsub_mask_name, peakvels_name,
-                       peakvelssub_mask_name, peakvelsub_cube_name)
+from paths import (fourteenB_HI_data_path, fourteenB_HI_file_dict)
 from galaxy_params import gal
 
 
-cube = SpectralCube.read(fourteenB_HI_data_path(cube_name))
+cube = SpectralCube.read(fourteenB_HI_file_dict['Cube'])
 
-mom1 = fits.open(fourteenB_HI_data_path(moment1_name))[0].data * u.m / u.s
-peakvels = fits.open(fourteenB_HI_data_path(peakvels_name))[0].data * u.m / u.s
+mom1 = fits.open(fourteenB_HI_file_dict["Moment1"])[0].data * u.m / u.s
+peakvels = fits.open(fourteenB_HI_file_dict["PeakVels"])[0].data * u.m / u.s
+rotmod_name = fourteenB_HI_data_path("diskfit_peakvels_noasymm_noradial_nowarp_output/rad.fitmod.fits")
+rot_model = fits.open(rotmod_name)[0].data * u.m / u.s
 
 pool = Pool(6)
 
 # Shift with the first moment
+log.info("Shifting w/ centroid")
+centroidsub_cube_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked.centroid_corrected.fits"
 cube_shifter(cube, mom1, gal.vsys, save_shifted=True,
              save_name=fourteenB_HI_data_path(centroidsub_cube_name,
                                               no_check=True),
              return_spectra=False, verbose=True, pool=pool)
 
 # Also shift the signal mask to match those shifted here.
-
-mask_cube = SpectralCube.read(fourteenB_HI_data_path(mask_name))
-
+log.info("Shifting mask w/ centroid")
+mask_cube = SpectralCube.read(fourteenB_HI_file_dict["Source_Mask"])
+centroidsub_mask_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked_source_mask.centroid_corrected.fits"
 cube_shifter(mask_cube, mom1, gal.vsys, save_shifted=True,
              save_name=fourteenB_HI_data_path(centroidsub_mask_name,
                                               no_check=True),
              return_spectra=False, verbose=True, pool=pool,
              is_mask=True)
 
-
 # Shift wrt. peak velocities
+log.info("Shifting w/ peak velocity")
+peakvelsub_cube_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked.peakvels_corrected.fits"
 cube_shifter(cube, peakvels, gal.vsys, save_shifted=True,
              save_name=fourteenB_HI_data_path(peakvelsub_cube_name,
                                               no_check=True),
              return_spectra=False, verbose=True, pool=pool)
 
 # Also shift the signal mask to match those shifted here.
-
-mask_cube = SpectralCube.read(fourteenB_HI_data_path(mask_name))
-
+log.info("Shifting mask w/ peak velocity")
+peakvelsub_mask_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked_source_mask.peakvels_corrected.fits"
 cube_shifter(mask_cube, peakvels, gal.vsys, save_shifted=True,
-             save_name=fourteenB_HI_data_path(peakvelssub_mask_name,
+             save_name=fourteenB_HI_data_path(peakvelsub_mask_name,
                                               no_check=True),
              return_spectra=False, verbose=True, pool=pool,
              is_mask=True)
 
+# Shift with the rotation model
+log.info("Shifting w/ rotation velocity")
+rotsub_cube_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked.rotation_corrected.fits"
+cube_shifter(cube, rot_model, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_path(rotsub_cube_name,
+                                              no_check=True),
+             return_spectra=False, verbose=True, pool=pool)
+
+# Also shift the signal mask to match those shifted here.
+log.info("Shifting mask w/ rotation velocity")
+rotsub_mask_name = "M33_14B-088_HI.clean.image.pbcov_gt_0.5_masked_source_mask.rotation_corrected.fits"
+cube_shifter(mask_cube, rot_model, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_path(rotsub_mask_name,
+                                              no_check=True),
+             return_spectra=False, verbose=True, pool=pool,
+             is_mask=True)
 pool.close()
 pool.join()
---
+++ 14B-088/HI/analysis/cube_shift_spectra_feathered.py
@@ -0,0 +1,87 @@
+
+'''
+Given a velocity surface, shift all spectra in the feathered cube.
+
+Mask versions shifted w.r.t:
+
+ * centroids
+ * peak velocities
+ * rotation model
+
+'''
+
+from spectral_cube import SpectralCube
+from astropy.io import fits
+import astropy.units as u
+from astropy import log
+from multiprocessing import Pool
+
+from cube_analysis.spectra_shifter import cube_shifter
+
+from paths import (fourteenB_HI_data_wGBT_path, fourteenB_wGBT_HI_file_dict)
+from galaxy_params import gal_feath as gal
+
+
+cube = SpectralCube.read(fourteenB_wGBT_HI_file_dict['Cube'])
+
+mom1 = fits.open(fourteenB_wGBT_HI_file_dict["Moment1"])[0].data * u.m / u.s
+peakvels = \
+    fits.open(fourteenB_wGBT_HI_file_dict["PeakVels"])[0].data * u.m / u.s
+rotmod_name = fourteenB_HI_data_wGBT_path("diskfit_peakvels_noasymm_noradial_nowarp_output/rad.fitmod.fits")
+rot_model = fits.open(rotmod_name)[0].data * u.m / u.s
+
+pool = Pool(6)
+
+# Shift with the first moment
+log.info("Shifting w/ centroid")
+centroidsub_cube_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.centroid_corrected.fits"
+cube_shifter(cube, mom1, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(centroidsub_cube_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool)
+
+# Also shift the signal mask to match those shifted here.
+log.info("Shifting mask w/ centroid")
+mask_cube = SpectralCube.read(fourteenB_wGBT_HI_file_dict["Source_Mask"])
+centroidsub_mask_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked_source_mask.centroid_corrected.fits"
+cube_shifter(mask_cube, mom1, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(centroidsub_mask_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool,
+             is_mask=True)
+
+# Shift wrt. peak velocities
+log.info("Shifting w/ peak velocity")
+peakvelsub_cube_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.peakvels_corrected.fits"
+cube_shifter(cube, peakvels, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(peakvelsub_cube_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool)
+
+# Also shift the signal mask to match those shifted here.
+log.info("Shifting mask w/ peak velocity")
+peakvelsub_mask_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked_source_mask.peakvels_corrected.fits"
+cube_shifter(mask_cube, peakvels, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(peakvelsub_mask_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool,
+             is_mask=True)
+
+# Shift with the rotation model
+log.info("Shifting w/ rotation velocity")
+rotsub_cube_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.rotation_corrected.fits"
+cube_shifter(cube, rot_model, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(rotsub_cube_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool)
+
+# Also shift the signal mask to match those shifted here.
+log.info("Shifting mask w/ rotation velocity")
+rotsub_mask_name = "M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked_source_mask.rotation_corrected.fits"
+cube_shifter(mask_cube, rot_model, gal.vsys, save_shifted=True,
+             save_name=fourteenB_HI_data_wGBT_path(rotsub_mask_name,
+                                                   no_check=True),
+             return_spectra=False, verbose=True, pool=pool,
+             is_mask=True)
+pool.close()
+pool.join()

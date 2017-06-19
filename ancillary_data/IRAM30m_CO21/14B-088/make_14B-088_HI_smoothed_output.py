df2791fb947ed197615f86fd310f8f958ed62386
e-koch <koch.eric.w@gmail.com>
2017-06-19 14:37:28 -0600

    Mask out the noisiest regions before smoothing; can lower the sigmas for the smoothed versions

--- ancillary_data/IRAM30m_CO21/14B-088/make_14B-088_HI_smoothed_output.py
+++ ancillary_data/IRAM30m_CO21/14B-088/make_14B-088_HI_smoothed_output.py
@@ -3,6 +3,8 @@
 Make a version of the CO(2-1) data at the resolution of the 14B-088 HI data
 '''
 
+import numpy as np
+import skimage.morphology as mo
 from spectral_cube import SpectralCube, Projection
 from spectral_cube.cube_utils import largest_beam
 from astropy.io import fits
@@ -18,8 +20,25 @@ vla_cube = SpectralCube.read(fourteenB_HI_file_dict["Cube"])
 
 co21_cube = SpectralCube.read(iram_co21_data_path("m33.co21_iram.fits"))
 
+# Mask out the really noisy edges so these don't get included when smoothing
+co21_noise = Projection.from_hdu(fits.open(iram_co21_data_path("m33.rms.fits"))[0])
+# NaNs are negative values in the map
+co21_noise[co21_noise.value < 0] = np.NaN
+# Initial mask below 0.04 K
+mask = co21_noise.value < 0.04
+# Remove holes
+mask = mo.remove_small_holes(mask)
+# Erode the edges by 10 pixels (chosen by-eye)
+mask = mo.erosion(mask, mo.square(10))
+co21_noise[~mask] = np.NaN
+
+# Save this version of the noise
+co21_noise.write(iram_co21_data_path("m33.rms.masked.fits", no_check=True),
+                 overwrite=True)
+
 # Smooth to the largest beam (they differ by tiny fractions anyway)
 large_beam = largest_beam(vla_cube.beams)
+co21_cube = co21_cube.with_mask(mask)
 smoothed_cube = co21_cube.convolve_to(large_beam)
 
 # Save to its own folder
@@ -27,22 +46,24 @@ if not os.path.exists(iram_co21_data_path("14B-088", no_check=True)):
     os.mkdir(iram_co21_data_path("14B-088", no_check=True))
 
 smooth_name = iram_co21_data_path("14B-088/m33.co21_iram.14B-088_HI_smoothed.fits", no_check=True)
-smoothed_cube.write(smooth_name)
+smoothed_cube.write(smooth_name, overwrite=True)
 
 # Now smooth the noise map
-co21_noise = Projection.from_hdu(fits.open(iram_co21_data_path("m33.rms.fits"))[0])
 co21_noise_smoothed = co21_noise.convolve_to(large_beam)
 smooth_noise_name = iram_co21_data_path("14B-088/m33.rms.14B-088_HI_smoothed.fits", no_check=True)
-# co21_noise_smoothed.write(smooth_noise_name)
+co21_noise_smoothed.write(smooth_noise_name, overwrite=True)
 
 # Find a signal mask and derive moment arrays
+# Require the same pixel size as the unsmoothed cube, but lower the min and
+# max SNR.
 run_pipeline(smooth_name, iram_co21_data_path("14B-088"),
              masking_kwargs={"method": "ppv_dilation",
                              "save_cube": True,
                              "noise_map": smooth_noise_name,
-                             "min_sig": 3,
-                             "max_sig": 5,
+                             "min_sig": 2.2,
+                             "max_sig": 4,
                              "min_pix": 27,
+                             "verbose": True
                              },
              moment_kwargs={"num_cores": 6,
                             "verbose": True})
@@ -63,16 +84,18 @@ co21_noise_smoothed_reproj = co21_noise_smoothed.reproject(vla_cube[0].header)
 smooth_reproj_noise_name = \
     iram_co21_data_path("14B-088_reproj/m33.rms.14B-088_HI_reproj.fits",
                         no_check=True)
+co21_noise_smoothed_reproj.write(smooth_reproj_noise_name, overwrite=True)
 
 run_pipeline(os.path.join(out_folder, "m33.co21_iram.14B-088_HI_reproj.fits"),
              out_folder,
              masking_kwargs={"method": "ppv_dilation",
                              "save_cube": True,
                              "noise_map": co21_noise_smoothed_reproj.value,
-                             "min_sig": 3,
-                             "max_sig": 5,
+                             "min_sig": 2.2,
+                             "max_sig": 4,
                              # the spatial cell of both grids are the same size
                              "min_pix": 27,
+                             "verbose": True
                              },
              moment_kwargs={"num_cores": 6,
                             "verbose": True})

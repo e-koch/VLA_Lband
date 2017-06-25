6c508d5a556e740cf0acb59aa341171e2ea04ecf
e-koch <koch.eric.w@gmail.com>
2017-06-25 13:31:23 -0600

    Add reprojecting in the spectral dim too

--- ancillary_data/IRAM30m_CO21/14B-088/hi_regrid_to_co.py
+++ ancillary_data/IRAM30m_CO21/14B-088/hi_regrid_to_co.py
@@ -31,3 +31,28 @@ reproject_cube(fourteenB_wGBT_HI_file_dict["Cube"],
                common_beam=False,
                verbose=True,
                chunk=80)
+
+# Make version reprojected in spectral dim as well.
+
+reproject_cube(fourteenB_HI_file_dict["Cube"],
+               iram_co21_data_path("m33.co21_iram.fits"),
+               "M33_14B-088_HI.clean.image.co21_iram_regrid_full.fits",
+               output_folder=fourteenB_HI_data_path("", no_check=True),
+               reproject_type='all',
+               save_spectral=False,
+               is_huge=True,
+               common_beam=False,
+               verbose=True,
+               chunk=80)
+
+
+reproject_cube(fourteenB_wGBT_HI_file_dict["Cube"],
+               iram_co21_data_path("m33.co21_iram.fits"),
+               "M33_14B-088_HI.clean.image.GBT_feathered.co21_iram_regrid_full.fits",
+               output_folder=fourteenB_HI_data_wGBT_path("", no_check=True),
+               reproject_type='all',
+               save_spectral=False,
+               is_huge=True,
+               common_beam=False,
+               verbose=True,
+               chunk=80)

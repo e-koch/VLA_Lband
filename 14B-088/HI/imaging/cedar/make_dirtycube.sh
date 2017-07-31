4aa19a7053e8ddab8809b90d97dcbca291af7b90
e-koch <koch.eric.w@gmail.com>
2017-07-31 13:25:00 -0600

    Move to scratch space before starting casa

--- 14B-088/HI/imaging/cedar/make_dirtycube.sh
+++ 14B-088/HI/imaging/cedar/make_dirtycube.sh
@@ -14,6 +14,9 @@ source /home/ekoch/preload.bash
 export scratch_path=/home/ekoch/scratch/
 export project_path=/home/ekoch/project/ekoch/
 
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
 $HOME/casa-release-5.0.0-218.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/14B-088/HI/imaging/cedar/HI_dirty_cube.py $scratch_path
 
 # Copy the dirty_cube folder into project space

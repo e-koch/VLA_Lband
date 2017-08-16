70a1562f0fe540c3a86ffa6af92e01625a1dd0c3
e-koch <koch.eric.w@gmail.com>
2017-08-16 17:12:57 -0600

    Try imaging using image tools instead of tclean

--- 14B-088/HI/imaging/cedar/make_dirtycube.sh
+++ 14B-088/HI/imaging/cedar/make_dirtycube.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH -t 4-00:00
+#SBATCH -t 60:00:00
 #SBATCH --mem=512GB
 #SBATCH --ntasks=32
+#SBATCH --nodes=1
 #SBATCH --job-name=M33_dirty_cube
 #SBATCH --output=casa-m33_dirtycube-%J.out
 export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
@@ -17,7 +18,7 @@ export project_path=/home/ekoch/project/ekoch/
 # Move to scratch space b/c casa write out the temporary files into the same folder
 cd $scratch_path
 
-$HOME/casa-release-5.0.0-218.el7/bin/mpicasa -n 32 $HOME/casa-release-5.0.0-218.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/14B-088/HI/imaging/cedar/HI_dirty_cube.py $scratch_path
+$HOME/casa-release-5.0.0-218.el7/bin/mpicasa -n 32 $HOME/casa-release-5.0.0-218.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/14B-088/HI/imaging/cedar/HI_dirty_cube_synthimage.py $scratch_path
 
 # Copy the dirty_cube folder into project space
 cp -R $scratch_path/dirty_cube $project_path

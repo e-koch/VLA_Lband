ec57279d37546fa5f7426cf11fe05ab60d351b88
e-koch <koch.eric.w@gmail.com>
2017-10-30 11:13:34 -0600

    Update CO HI velocity offset figures

--- 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
+++ 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
@@ -9,7 +9,7 @@ from corner import hist2d
 from os.path import join, exists
 import os
 
-from paths import (fourteenB_HI_file_dict, fourteenB_wGBT_HI_file_dict,
+from paths import (fourteenB_wGBT_HI_file_dict,
                    iram_co21_14B088_data_path,
                    allfigs_path)
 from plotting_styles import onecolumn_figure, default_figure, twocolumn_figure
@@ -27,21 +27,15 @@ fig_path = join(allfigs_path(""), "co_vs_hi")
 if not exists(fig_path):
     os.mkdir(fig_path)
 
-hi_mom1 = fits.open(fourteenB_HI_file_dict['Moment1'])[0].data / 1000.
-hi_peakvels = fits.open(fourteenB_HI_file_dict['PeakVels'])[0].data / 1000.
-hi_peaktemp = fits.open(fourteenB_HI_file_dict['PeakTemp'])[0].data
-
-hi_feath_mom1 = fits.open(fourteenB_wGBT_HI_file_dict['Moment1'])[0].data / 1000.
-hi_feath_peakvels = fits.open(fourteenB_wGBT_HI_file_dict['PeakVels'])[0].data / 1000.
-hi_feath_peaktemp = fits.open(fourteenB_wGBT_HI_file_dict['PeakTemp'])[0].data
+hi_mom1 = fits.open(fourteenB_wGBT_HI_file_dict['Moment1'])[0].data / 1000.
+hi_peakvels = fits.open(fourteenB_wGBT_HI_file_dict['PeakVels'])[0].data / 1000.
+hi_peaktemp = fits.open(fourteenB_wGBT_HI_file_dict['PeakTemp'])[0].data
 
 co_mom1 = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.mom1.fits"))[0].data / 1000.
 co_peakvels = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.peakvels.fits"))[0].data / 1000.
 co_peaktemp = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.peaktemps.fits"))[0].data
 
 good_pts = np.logical_and(np.isfinite(hi_mom1), np.isfinite(co_mom1))
-good_pts_feath = np.logical_and(np.isfinite(hi_feath_mom1),
-                                np.isfinite(co_mom1))
 
 onecolumn_figure(font_scale=1.1)
 
@@ -51,7 +45,7 @@ hist2d(np.abs((co_mom1 - hi_mom1)[good_pts]), co_peaktemp[good_pts],
        range=[(0.0, 30.0),
               (0.0, 1.05 * np.max(co_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"|V$_\mathrm{cent, CO}$ - V$_\mathrm{cent, HI}$| (km/s)")
+plt.xlabel(r"$|V_\mathrm{cent, CO} - V_\mathrm{cent, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -64,41 +58,12 @@ hist2d(np.abs((co_mom1 - hi_mom1)[good_pts]), hi_peaktemp[good_pts],
        range=[(0.0, 30.0),
               (0.0, 1.05 * np.max(hi_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"|V$_\mathrm{cent, CO}$ - V$_\mathrm{cent, HI}$| (km/s)")
+plt.xlabel(r"$|V_\mathrm{cent, CO} - V_\mathrm{cent, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
 plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_centroid_velocity_offset.pdf")))
 plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_centroid_velocity_offset.png")))
-plt.close()
-
-# Feathered Mom1 comparisons
-hist2d(np.abs((co_mom1 - hi_feath_mom1)[good_pts_feath]),
-       co_peaktemp[good_pts_feath],
-       bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 50.0),
-              (0.0, 1.05 * np.max(co_peaktemp[good_pts_feath]))])
-plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"|V$_\mathrm{cent, CO}$ - V$_\mathrm{cent, HI}$| (km/s)")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig(allfigs_path(join(fig_path, "co21_Tpeak_centroid_velocity_offset_feather.pdf")))
-plt.savefig(allfigs_path(join(fig_path, "co21_Tpeak_centroid_velocity_offset_feather.png")))
-plt.close()
-
-hist2d(np.abs((co_mom1 - hi_feath_mom1)[good_pts_feath]),
-       hi_feath_peaktemp[good_pts_feath],
-       bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 30.0),
-              (0.0, 1.05 * np.max(hi_peaktemp[good_pts]))])
-plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"|V$_\mathrm{cent, CO}$ - V$_\mathrm{cent, HI}$| (km/s)")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_centroid_velocity_offset_feather.pdf")))
-plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_centroid_velocity_offset_feather.png")))
 plt.close()
 
 # Peak Velocity Comparisons
@@ -107,7 +72,7 @@ hist2d(np.abs((co_peakvels - hi_peakvels)[good_pts]), co_peaktemp[good_pts],
        range=[(0.0, 50.0),
               (0.0, 1.05 * np.max(co_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"|V$_\mathrm{peak, CO}$ - V$_\mathrm{peak, HI}$| (km/s)")
+plt.xlabel(r"$|V_\mathrm{peak, CO} - V_\mathrm{peak, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -120,7 +85,7 @@ hist2d(np.abs((co_peakvels - hi_peakvels)[good_pts]), hi_peaktemp[good_pts],
        range=[(0.0, 50.0),
               (0.0, 1.05 * np.max(hi_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"|V$_\mathrm{peak, CO}$ - V$_\mathrm{peak, HI}$| (km/s)")
+plt.xlabel(r"$|V_\mathrm{peak, CO} - V_\mathrm{peak, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -129,37 +94,8 @@ plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_peakvel_velocity_offset.png"))
 plt.close()
 
 
-# Feathered peak vels comparisons
-hist2d(np.abs((co_peakvels - hi_feath_peakvels)[good_pts_feath]),
-       co_peaktemp[good_pts_feath],
-       bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 50.0),
-              (0.0, 1.05 * np.max(co_peaktemp[good_pts_feath]))])
-plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"|V$_\mathrm{peak, CO}$ - V$_\mathrm{peak, HI}$| (km/s)")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig(allfigs_path(join(fig_path, "co21_Tpeak_peakvel_velocity_offset_feather.pdf")))
-plt.savefig(allfigs_path(join(fig_path, "co21_Tpeak_peakvel_velocity_offset_feather.png")))
-plt.close()
-
-hist2d(np.abs((co_peakvels - hi_feath_peakvels)[good_pts_feath]),
-       hi_feath_peaktemp[good_pts_feath],
-       bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 50.0),
-              (0.0, 1.05 * np.max(hi_feath_peaktemp[good_pts]))])
-plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"|V$_\mathrm{peak, CO}$ - V$_\mathrm{peak, HI}$| (km/s)")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_peakvel_velocity_offset_feather.pdf")))
-plt.savefig(allfigs_path(join(fig_path, "hi_Tpeak_peakvel_velocity_offset_feather.png")))
-plt.close()
-
 # Where do the outliers occur
-moment0 = Projection.from_hdu(fits.open(fourteenB_HI_file_dict["Moment0"])[0])
+moment0 = Projection.from_hdu(fits.open(fourteenB_wGBT_HI_file_dict["Moment0"])[0])
 
 moment0_Kkm_s = moment0.value * moment0.beam.jtok(hi_freq).value / 1000.
 
@@ -180,11 +116,11 @@ ax.set_xlabel("RA (J2000)")
 
 # Where are the velocity outliers spatially?
 # Assign colours to points based on their percentile in the distribution
-vel_diff = np.abs(co_mom1 - hi_feath_mom1)
+vel_diff = np.abs(co_mom1 - hi_mom1)
 percs = np.nanpercentile(vel_diff,
                          [85, 95, 99, 99.5])
-labels = ["85%", "95%", "99%", "99.5%"]
-labels = ["> {0} km/s ({1})".format(round(perc, 1), label)
+labels = ["85\%", "95\%", "99\%", "99.5\%"]
+labels = [r"$>$ {0} km/s ({1})".format(round(perc, 1), label)
           for perc, label in zip(percs, labels)]
 
 cols = ["D", "s", "o", "^"]

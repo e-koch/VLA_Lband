841297da1f44706a966056d899ee143034faa766
e-koch <koch.eric.w@gmail.com>
2017-11-04 14:25:41 -0600

    Add separate outlier plot for peak and centroid velocities

--- 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
+++ 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
@@ -35,17 +35,21 @@ co_mom1 = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.mom1.fi
 co_peakvels = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.peakvels.fits"))[0].data / 1000.
 co_peaktemp = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI.peaktemps.fits"))[0].data
 
-good_pts = np.logical_and(np.isfinite(hi_mom1), np.isfinite(co_mom1))
+co_mask = fits.open(iram_co21_14B088_data_path("m33.co21_iram.14B-088_HI_source_mask.fits"))[0]
+
+good_co_pts = co_mask.data.sum(0) >= 2
+
+good_pts = np.logical_and(np.isfinite(hi_mom1), good_co_pts)
 
 onecolumn_figure(font_scale=1.1)
 
 # Mom1 comparisons
 hist2d(np.abs((co_mom1 - hi_mom1)[good_pts]), co_peaktemp[good_pts],
        bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 30.0),
+       range=[(0.0, 25.0),
               (0.0, 1.05 * np.max(co_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"$|V_\mathrm{cent, CO} - V_\mathrm{cent, HI}|$ (km/s)")
+plt.xlabel(r"$|V_{\rm cent, CO} - V_{\rm cent, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -55,10 +59,10 @@ plt.close()
 
 hist2d(np.abs((co_mom1 - hi_mom1)[good_pts]), hi_peaktemp[good_pts],
        bins=16, data_kwargs={"alpha": 0.6},
-       range=[(0.0, 30.0),
+       range=[(0.0, 25.0),
               (0.0, 1.05 * np.max(hi_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"$|V_\mathrm{cent, CO} - V_\mathrm{cent, HI}|$ (km/s)")
+plt.xlabel(r"$|V_{\rm cent, CO} - V_{\rm cent, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -72,7 +76,7 @@ hist2d(np.abs((co_peakvels - hi_peakvels)[good_pts]), co_peaktemp[good_pts],
        range=[(0.0, 50.0),
               (0.0, 1.05 * np.max(co_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, CO}$ (K)")
-plt.xlabel(r"$|V_\mathrm{peak, CO} - V_\mathrm{peak, HI}|$ (km/s)")
+plt.xlabel(r"$|V_{\rm peak, CO} - V_{\rm peak, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -85,7 +89,7 @@ hist2d(np.abs((co_peakvels - hi_peakvels)[good_pts]), hi_peaktemp[good_pts],
        range=[(0.0, 50.0),
               (0.0, 1.05 * np.max(hi_peaktemp[good_pts]))])
 plt.ylabel(r"T$_\mathrm{peak, HI}$ (K)")
-plt.xlabel(r"$|V_\mathrm{peak, CO} - V_\mathrm{peak, HI}|$ (km/s)")
+plt.xlabel(r"$|V_{\rm peak, CO} - V_{\rm peak, HI}|$ (km/s)")
 plt.grid()
 plt.tight_layout()
 
@@ -124,6 +128,46 @@ labels = [r"$>$ {0} km/s ({1})".format(round(perc, 1), label)
           for perc, label in zip(percs, labels)]
 
 cols = ["D", "s", "o", "^"]
+
+nonan_vel_diff = vel_diff.copy()
+nonan_vel_diff[np.isnan(nonan_vel_diff)] = 0.0
+
+for perc, col, label in zip(percs, cols, labels):
+    ypos, xpos = np.where(np.abs(nonan_vel_diff) > perc)
+    plt.plot(xpos, ypos, col, label=label)
+
+plt.legend(loc='lower left', frameon=True)
+
+cbar = plt.colorbar(im)
+cbar.set_label(r"Integrated Intensity (K km s$^{-1}$)")
+
+plt.savefig(allfigs_path(join(fig_path, "co21_HI_centroid_offset_outliers.pdf")))
+plt.savefig(allfigs_path(join(fig_path, "co21_HI_centroid_offset_outliers.png")))
+plt.close()
+
+# Peak velocity outliers
+ax = plt.subplot(111, projection=moment0.wcs.celestial)
+im = ax.imshow(moment0_Kkm_s,
+               origin='lower',
+               interpolation='nearest',
+               norm=ImageNormalize(vmin=-0.001,
+                                   vmax=np.nanmax(moment0_Kkm_s),
+                                   stretch=AsinhStretch()))
+ax.set_ylabel("DEC (J2000)")
+ax.set_xlabel("RA (J2000)")
+# ax.set_ylim([260, 900])
+# ax.set_xlim([130, 620])
+
+# Where are the velocity outliers spatially?
+# Assign colours to points based on their percentile in the distribution
+vel_diff = np.abs(co_peakvels - hi_peakvels)
+percs = np.nanpercentile(vel_diff,
+                         [85, 95, 99, 99.5, 99.9])
+labels = ["85\%", "95\%", "99\%", "99.5\%", "99.9\%"]
+labels = [r"$>$ {0} km/s ({1})".format(round(perc, 1), label)
+          for perc, label in zip(percs, labels)]
+
+cols = ["D", "s", "o", "^", '1']
 
 nonan_vel_diff = vel_diff.copy()
 nonan_vel_diff[np.isnan(nonan_vel_diff)] = 0.0

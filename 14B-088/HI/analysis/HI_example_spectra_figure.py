978d12974146f738a896e9e2f6e81266f22b7dd9
e-koch <koch.eric.w@gmail.com>
2017-12-18 17:08:03 -0800

    Update the example spectra figure

--- 14B-088/HI/analysis/HI_example_spectra_figure.py
+++ 14B-088/HI/analysis/HI_example_spectra_figure.py
@@ -9,14 +9,21 @@ from spectral_cube import SpectralCube, Projection
 import matplotlib.pyplot as plt
 from astropy.io import fits
 import astropy.units as u
+from astropy.wcs import WCS
+from astropy.wcs.utils import proj_plane_pixel_area
+from radio_beam import Beam
 import os
 from os.path import join as osjoin
 import seaborn as sb
+import matplotlib.gridspec as gridspec
+from astropy.visualization import AsinhStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+import numpy as np
 
 from paths import (fourteenB_HI_file_dict, fourteenB_wGBT_HI_file_dict,
                    data_path, allfigs_path)
 from plotting_styles import twocolumn_figure, default_figure
-from constants import hi_freq
+from constants import hi_freq, hi_coldens_Kkms
 
 
 figure_folder = allfigs_path("HI_maps")
@@ -31,17 +38,55 @@ comb_cube = SpectralCube.read(fourteenB_wGBT_HI_file_dict['Cube'])
 peak_vels = fits.open(fourteenB_wGBT_HI_file_dict['PeakVels'])[0]
 peak_temps = fits.open(fourteenB_wGBT_HI_file_dict['PeakTemp'])[0]
 
-example_posns = [(924, 445), (986, 505), (624, 955), (651, 735),
+# example_posns = [(924, 445), (986, 505), (624, 955), (651, 735),
+#                  (659, 745), (802, 640)]
+# example_labels = ["a", "b", "c", "d", "e", "f"]
+
+example_posns = [(986, 505), (924, 445),
                  (659, 745), (802, 640)]
-example_labels = ["a", "b", "c", "d", "e", "f"]
+example_labels = ["a", "b", "c", "d",]
+
 
 col_pal = sb.color_palette()
 
 twocolumn_figure()
 
-fig, axs = plt.subplots(3, 2)
+gs = gridspec.GridSpec(4, 4)
 
-for i, ax in enumerate(axs.ravel()):
+# Plot the column density map with locations of the spectra
+moment0_feath = fits.open(fourteenB_wGBT_HI_file_dict["Moment0"])[0]
+moment0_wcs = WCS(moment0_feath.header)
+
+beam = Beam.from_fits_header(moment0_feath.header)
+moment0_Kkm_s_feath = beam.jtok(hi_freq).value * moment0_feath.data / 1000.
+moment0_coldens_feath = moment0_Kkm_s_feath * hi_coldens_Kkms.value
+
+ax1 = plt.subplot(gs[:, :2], projection=moment0_wcs)
+im = ax1.imshow(moment0_coldens_feath,
+                origin='lower',
+                interpolation='nearest',
+                norm=ImageNormalize(vmin=-0.001 * hi_coldens_Kkms.value,
+                                    vmax=np.nanmax(moment0_coldens_feath),
+                                    stretch=AsinhStretch()))
+ax1.set_ylabel("")
+ax1.set_xlabel("")
+
+pixscale = np.sqrt(proj_plane_pixel_area(moment0_wcs))
+
+ax1.add_patch(beam.ellipse_to_plot(int(0.123 * moment0_feath.shape[0]),
+                                  int(0.05 * moment0_feath.shape[1]), pixscale))
+
+col_pal = sb.color_palette()
+for j, (y, x) in enumerate(example_posns):
+    ax1.scatter(x, y, color=col_pal[j], edgecolor='w',
+                marker='D', s=70)
+
+ax2 = plt.subplot(gs[0, 2:])
+ax3 = plt.subplot(gs[1, 2:])
+ax4 = plt.subplot(gs[2, 2:])
+ax5 = plt.subplot(gs[3, 2:])
+
+for i, ax in enumerate([ax2, ax3, ax4, ax5]):
 
     y, x = example_posns[i]
     print(example_posns[i])
@@ -53,33 +98,39 @@ for i, ax in enumerate(axs.ravel()):
     vla_spec = vla_cube[:, y, x].value * arch_cube.beam.jtok(hi_freq).value * u.K
     comb_spec = comb_cube[:, y, x].value * arch_cube.beam.jtok(hi_freq).value * u.K
 
-    ax.plot(arch_cube.spectral_axis.value / 1000., arch_spec.value, label='Archival',
+    ax.plot((arch_cube.spectral_axis.value - peak_vels.data[y, x]) / 1000., arch_spec.value, label='Archival',
             drawstyle='steps-mid')
-    ax.plot(vla_cube.spectral_axis.value / 1000., vla_spec, label='VLA',
-            drawstyle='steps-mid')
-    ax.plot(comb_cube.spectral_axis.value / 1000., comb_spec, label='VLA+GBT',
+    # ax.plot(vla_cube.spectral_axis.value / 1000., vla_spec, label='VLA',
+    #         drawstyle='steps-mid')
+    ax.plot((comb_cube.spectral_axis.value - peak_vels.data[y, x]) / 1000., comb_spec, label='VLA+GBT',
             drawstyle='steps-mid')
     ax.grid()
     ax.axhline(0, color='k', alpha=0.5, linestyle='--')
-    if i == 0:
-        ax.legend(loc='upper left', frameon=True)
-
-    if i == 1 or i == 3 or i == 5:
-        ax.yaxis.set_label_position("right")
-        ax.yaxis.tick_right()
-        ax.yaxis.set_ticks_position('both')
-
-    if i == 2:
-        ax.set_ylabel(r"$T_{\rm HI}$ (K)")
-
-    if i == 4 or i == 5:
-        ax.set_xlabel(r"Velocity (km/s)")
 
     low_lim = max(vla_cube.spectral_axis.min().value,
                   peak_vels.data[y, x] - 50000)
     high_lim = min(vla_cube.spectral_axis.max().value,
                    peak_vels.data[y, x] + 50000)
-    ax.set_xlim([low_lim / 1000., high_lim / 1000.])
+    ax.set_xlim([-50000 / 1000., 50000 / 1000.])
+
+    # if i == 0:
+    #     ax.legend(loc='upper left', frameon=True)
+
+    # if i == 1 or i == 3 or i == 5:
+    # if i == 2 or i == 3:
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    ax.yaxis.set_ticks_position('both')
+
+    # if i == 2:
+    # if i == 2 or i == 3:
+    ax.set_ylabel(r"$T_{\rm HI}$ (K)")
+
+    # if i == 4 or i == 5:
+    if i == 3:
+        ax.set_xlabel(r"Velocity (km/s)")
+    else:
+        ax.axes.xaxis.set_ticklabels([])
 
     ax.text(0.9, 0.8, "({})".format(example_labels[i]),
             horizontalalignment='center',
@@ -92,8 +143,10 @@ for i, ax in enumerate(axs.ravel()):
 
 plt.tight_layout()
 
-fig.savefig(osjoin(figure_folder, "example_HI_spectra.png"))
-fig.savefig(osjoin(figure_folder, "example_HI_spectra.pdf"))
+print(argh)
+
+plt.savefig(osjoin(figure_folder, "example_HI_spectra.png"))
+plt.savefig(osjoin(figure_folder, "example_HI_spectra.pdf"))
 
 plt.close()
 

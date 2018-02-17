e29eafa08b3854bc44cd54b27d0e51467ba78469
e-koch <koch.eric.w@gmail.com>
2018-02-16 17:26:58 -0700

    Add N/S labels

--- 14B-088/HI/analysis/HI_offrotation_components.py
+++ 14B-088/HI/analysis/HI_offrotation_components.py
@@ -45,86 +45,86 @@ split = Nrows * Ncols
 beam = cube.beam
 
 # Split the 48 slabs into 4 12-panel figures
-for n in range(5):
-    plt.figure(1, figsize=(8, 11)).clf()
+# for n in range(5):
+#     plt.figure(1, figsize=(8, 11)).clf()
 
-    vchans = vel_bins[n * split: (n + 1) * split + 1]
+#     vchans = vel_bins[n * split: (n + 1) * split + 1]
 
-    fig, ax = plt.subplots(Nrows, Ncols,
-                           sharex=True,
-                           sharey=True, num=1)
+#     fig, ax = plt.subplots(Nrows, Ncols,
+#                            sharex=True,
+#                            sharey=True, num=1)
 
-    # Convert to K km/s, but beam equivalency doesn't allow it. Doing it ad hoc
-    layers = \
-        [rotsub_cube.spectral_slab(start * u.km / u.s, end * u.km / u.s).moment0().value *
-         beam.jtok(hi_freq) / 1000. * u.km / u.s
-         for start, end in zip(vchans[:-1], vchans[1:])]
-    # Determine the maximum value to display
-    mx = np.max([np.nanmax(x).value for x in layers])
+#     # Convert to K km/s, but beam equivalency doesn't allow it. Doing it ad hoc
+#     layers = \
+#         [rotsub_cube.spectral_slab(start * u.km / u.s, end * u.km / u.s).moment0().value *
+#          beam.jtok(hi_freq) / 1000. * u.km / u.s
+#          for start, end in zip(vchans[:-1], vchans[1:])]
+#     # Determine the maximum value to display
+#     mx = np.max([np.nanmax(x).value for x in layers])
 
-    for ii, (v1, v2) in enumerate(zip(vchans[:-1], vchans[1:])):
-        layer = layers[ii]
+#     for ii, (v1, v2) in enumerate(zip(vchans[:-1], vchans[1:])):
+#         layer = layers[ii]
 
-        y, x = np.unravel_index(ii, (Nrows, Ncols))
+#         y, x = np.unravel_index(ii, (Nrows, Ncols))
 
-        im = ax[y, x].imshow(layer.value, origin='lower',
-                             norm=ImageNormalize(vmin=-0.001,
-                                                 vmax=mx,
-                                                 stretch=AsinhStretch()),
-                             cmap=plt.cm.gray_r)
-        # vel1 = cube.spectral_axis.to(u.km / u.s)[v1].value
-        # vel2 = cube.spectral_axis.to(u.km / u.s)[v2].value
-        ax[y, x].annotate("${0:.0f}<v<{1:.0f}$".format(v1, v2),
-                          (0.53, 0.9),
-                          xycoords='axes fraction', color='k',
-                          fontsize=8)
+#         im = ax[y, x].imshow(layer.value, origin='lower',
+#                              norm=ImageNormalize(vmin=-0.001,
+#                                                  vmax=mx,
+#                                                  stretch=AsinhStretch()),
+#                              cmap=plt.cm.gray_r)
+#         # vel1 = cube.spectral_axis.to(u.km / u.s)[v1].value
+#         # vel2 = cube.spectral_axis.to(u.km / u.s)[v2].value
+#         ax[y, x].annotate("${0:.0f}<v<{1:.0f}$".format(v1, v2),
+#                           (0.53, 0.9),
+#                           xycoords='axes fraction', color='k',
+#                           fontsize=8)
 
-    # fig.subplots_adjust(right=0.8)
-    cbar_ax = fig.add_axes([1.00, 0.05, 0.04, 0.9])
-    cb = fig.colorbar(im, cax=cbar_ax)
-    cb.set_label("K km s$^{-1}$")
+#     # fig.subplots_adjust(right=0.8)
+#     cbar_ax = fig.add_axes([1.00, 0.05, 0.04, 0.9])
+#     cb = fig.colorbar(im, cax=cbar_ax)
+#     cb.set_label("K km s$^{-1}$")
 
-    plt.subplots_adjust(hspace=0,
-                        wspace=0)
+#     plt.subplots_adjust(hspace=0,
+#                         wspace=0)
 
-    for i in range(Nrows):
-        for j in range(Ncols):
-            if i == 0:
-                ax[i, j].xaxis.set_ticks_position('top')
-                plt.setp(ax[i, j].get_xticklabels(), visible=False)
-                ax[i, j].xaxis.set_ticklabels([])
-            elif i == Nrows - 1:
-                ax[i, j].xaxis.set_ticks_position('bottom')
-                plt.setp(ax[i, j].get_xticklabels(), visible=True)
-            else:
-                ax[i, j].xaxis.set_ticks_position('none')
-                plt.setp(ax[i, j].get_xticklabels(), visible=False)
-                ax[i, j].xaxis.set_ticklabels([])
+#     for i in range(Nrows):
+#         for j in range(Ncols):
+#             if i == 0:
+#                 ax[i, j].xaxis.set_ticks_position('top')
+#                 plt.setp(ax[i, j].get_xticklabels(), visible=False)
+#                 ax[i, j].xaxis.set_ticklabels([])
+#             elif i == Nrows - 1:
+#                 ax[i, j].xaxis.set_ticks_position('bottom')
+#                 plt.setp(ax[i, j].get_xticklabels(), visible=True)
+#             else:
+#                 ax[i, j].xaxis.set_ticks_position('none')
+#                 plt.setp(ax[i, j].get_xticklabels(), visible=False)
+#                 ax[i, j].xaxis.set_ticklabels([])
 
-            if j == 0:
-                ax[i, j].yaxis.set_ticks_position('left')
-            elif j == Ncols - 1:
-                ax[i, j].yaxis.set_ticks_position('right')
-                plt.setp(ax[i, j].get_yticklabels(), visible=False)
-                ax[i, j].yaxis.set_ticklabels([])
-            else:
-                ax[i, j].yaxis.set_ticks_position('none')
-                plt.setp(ax[i, j].get_yticklabels(), visible=False)
-                ax[i, j].yaxis.set_ticklabels([])
+#             if j == 0:
+#                 ax[i, j].yaxis.set_ticks_position('left')
+#             elif j == Ncols - 1:
+#                 ax[i, j].yaxis.set_ticks_position('right')
+#                 plt.setp(ax[i, j].get_yticklabels(), visible=False)
+#                 ax[i, j].yaxis.set_ticklabels([])
+#             else:
+#                 ax[i, j].yaxis.set_ticks_position('none')
+#                 plt.setp(ax[i, j].get_yticklabels(), visible=False)
+#                 ax[i, j].yaxis.set_ticklabels([])
 
-    plt.subplots_adjust(hspace=0,
-                        wspace=0)
+#     plt.subplots_adjust(hspace=0,
+#                         wspace=0)
 
-    plt.draw()
+#     plt.draw()
 
-    # raw_input("Next plot?")
-    plt.savefig(allfigs_path("HI_maps/M33_rotsub_channels_{}.pdf".format(n)),
-                bbox_inches="tight")
-    plt.savefig(allfigs_path("HI_maps/M33_rotsub_channels_{}.png".format(n)),
-                bbox_inches="tight")
-    plt.clf()
+#     # raw_input("Next plot?")
+#     plt.savefig(allfigs_path("HI_maps/M33_rotsub_channels_{}.pdf".format(n)),
+#                 bbox_inches="tight")
+#     plt.savefig(allfigs_path("HI_maps/M33_rotsub_channels_{}.png".format(n)),
+#                 bbox_inches="tight")
+#     plt.clf()
 
-plt.close()
+# plt.close()
 
 # Now focus on individual features
 
@@ -181,7 +181,7 @@ fig1.tick_labels.set_yformat('dd:mm')
 fig1.hide_axis_labels()
 fig1.colorbar.set_ticks([0, 20, 60, 100, 140])
 fig1.colorbar.set_font(size=11)
-fig1.add_label(0.05, 0.05, '-150<v<-60 km/s ', relative=True,
+fig1.add_label(0.05, 0.05, '-150 < v < -60 km/s ', relative=True,
                bbox={"boxstyle": "square", "facecolor": "w"},
                size=10, horizontalalignment='left',
                verticalalignment='bottom')
@@ -200,10 +200,20 @@ fig2.colorbar.set_axis_label_text('Integrated Intensity (K km/s)')
 fig2.colorbar.set_axis_label_font(size=11, weight='normal')
 fig2.colorbar.set_ticks([0, 20, 60, 100, 140])
 fig2.colorbar.set_font(size=11)
-fig2.add_label(0.05, 0.05, '50<v<140 km/s ', relative=True,
+fig2.add_label(0.05, 0.05, '50 < v < 140 km/s ', relative=True,
                bbox={"boxstyle": "square", "facecolor": "w"},
                size=10, horizontalalignment='left',
                verticalalignment='bottom')
+
+# Add N/S labels too
+fig1.add_label(0.95, 0.95, 'South', relative=True,
+               bbox={"boxstyle": "square", "facecolor": "w"},
+               size=12, horizontalalignment='right',
+               verticalalignment='top')
+fig2.add_label(0.95, 0.95, 'North', relative=True,
+               bbox={"boxstyle": "square", "facecolor": "w"},
+               size=12, horizontalalignment='right',
+               verticalalignment='top')
 
 plt.tight_layout()
 

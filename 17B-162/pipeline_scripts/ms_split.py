4d14a414119d041ac1ced3d55baedf62b9817812
e-koch <koch.eric.w@gmail.com>
2018-04-05 10:16:10 -0600

    Alter which fields are kept in the split MSs

--- 17B-162/pipeline_scripts/ms_split.py
+++ 17B-162/pipeline_scripts/ms_split.py
@@ -19,9 +19,6 @@ importasdm(asdm=mySDM, vis=ms_active, ocorr_mode='co',
            outfile='{}.flagonline.txt'.format(mySDM),
            createmms=parallel_run)
 
-line_run = False
-cont_run = False
-
 parentdir = os.getcwd().split("/")[-1]
 
 lines_folder = parentdir + '_speclines'
@@ -30,17 +27,17 @@ if not os.path.exists(lines_folder):
 
 # Copy the cont.dat file from the repo
 source_path = os.path.expanduser("~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/cont.dat")
-copyfile(source_path, lines_folder)
+copyfile(source_path, lines_folder + "/cont.dat")
 
 split(vis=ms_active,
       outputvis=lines_folder + "/" + mySDM + ".speclines.ms",
-      spw="8~17", datacolumn='DATA',
-      field="0137+331=3C48,M33_2,M33_14,M33_6,M33_7_center,M33_12,M33_11,M33_8")
+      spw="8~17", datacolumn='DATA', field="")
 
-line_run = True
-
-line_ms = mySDM + ".speclines"
-
+# While it would be nice to remove the pol cal scans here, the pipeline
+# will fail when running fluxboot because there is no other calibration
+# field to transfer to! We can avoid this by just keeping all of the
+# fields, even if they aren't used.
+    # field="0137+331=3C48,M33_2,M33_14,M33_6,M33_7_center,M33_12,M33_11,M33_8")
 
 cont_folder = parentdir + '_continuum'
 if not os.path.exists(cont_folder):
@@ -50,5 +47,3 @@ split(vis=ms_active,
       outputvis=cont_folder + "/" + mySDM + ".continuum.ms",
       spw="0~7", datacolumn='DATA',
       field="")
-
-cont_ms = mySDM + ".continuum"

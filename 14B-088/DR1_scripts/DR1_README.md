b21ed84101f646502462af68ef4eae882c2372fa
e-koch <koch.eric.w@gmail.com>
2018-05-01 16:31:33 -0600

    Scripts for the first DR

---
+++ 14B-088/DR1_scripts/DR1_README.md
@@ -0,0 +1,33 @@
+
+# First Data Release from the M33 L-band VLA Project
+# Data presented in from Koch et al. (2018)
+# Paper DOI: 
+# Code link: https://github.com/ekoch/VLA_Lband/
+
+This is the first data release of the M33 L-band VLA project. This release includes most of the spectral-line data taking in VLA project 14B-088.
+
+# HI Data
+
+* `M33_14B-088_HI.clean.image.GBT_feathered.2.6kms.fits` - HI cube with a velocity resolution of 2.6 km/s. **Note:** This is not the cube version presented in the paper linked above. The 200 m/s cube will be released in the next data release.
+* `M33_14B-088_pbcov.fits` - The primary beam coverage of the HI data.
+* Moment arrays - These are derived from the 200 m/s cube with a primary beam cut-off of 0.5 applied.
+* `rotation_curves` - Contains the DISKFIT outputs for fitting a circular rotation curve to the peak velocity surface. `rad.out.csv` contains the circular velocities and errors in each bin. `rad.out.params.csv` contains the fitted disk parameters. FITS files with `fit` in the name are the fitted model or residual compared to fitting a Brandt rotation curve, as described in the paper. `rad.mod.fits` and `rad.res.fits` are the model and residual surfaces directly from the DISKFIT model.
+* `stacked_spectra` - A series of stacked spectra over the entire disk, in radial bins, in peak temperature bins, and their respective fits. Note that the stacked profiles are given only for the feathered VLA+GBT data, but the fit tables have fits to both the feathered data and the VLA-only data. Since we consider the former to be more scientifically useful, the VLA-only stacked profiles are not given here. Contact the authors for the VLA-only stacked profiles.
+    - `NAME_stacked.fits` - Stacked spectra over the entire disk (complete out to a radius of ~8 kc). There are files for aligning the spectra based on the rotation, centroid and peak velocity surfaces.
+    - `hi_gaussian_totalprof_hwhm_fits.csv` - Fit parameters to the previous stacked profiles. See the linked paper for a description of the fit parameters. Includes fits to the VLA-only and VLA+GBT stacked profiles. The latter have "feath" in their column names.
+    - `NAME_100pc.fits` - Stacked spectra in 100 pc radial bins (in the frame of the galaxy). Files includes stacks over the entire radial bin and those limited to the northern and southern halves, respectively.
+    - `hi_gaussian_hwhm_totalprof_fits_radial.csv` - Fits to the radially-stacked profiles. See the linked paper for a description of the fit parameters. Includes fits to the VLA-only and VLA+GBT stacked profiles. The latter have "feath" in their column names.
+    - `NAME_5percentile.fits` - Stacks in 5-percentile intervals of the peak temperature distribution.
+    -  `hi_hwhm_totalprof_fits_peak_5percentile_feather.csv` - Fits to the peak temperature stacked profiles. See the linked paper for a description of the fit parameters. Only includes fits to the VLA+GBT stacked profiles.
+
+# OH Data
+
+Note that data for the separately-published OH-1665 maser detection can be found [here](http://apps.canfar.net/storage/list/ekoch/M33/VLA/Koch2018_OH_maser).
+
+Naturally-weighted data cubes with 1.5 km/s velocity channels are given for each of the 4 transitions.  These are dirty images with no deconvolution applied. There is only one clear detection in the 1665 MHz line (see above).
+
+# RRL Data
+
+Naturally-weighted data cubes with 10 km/s velocity channels are given for 5 Hydrogen radio-recombination lines. These are dirty images with no deconvolution applied. There are no clear detections.
+
+Note that some of the cubes show artifacts from unflagged RFI in some pointings. These are left as is since the field with the highest coverage, which covers the giant HII region NGC 604, was found to show no emission, making it unlikely other regions would since it is one of the brightest Halpha sources in the galaxy
---
+++ 14B-088/DR1_scripts/canfar_dr1_copy.sh
@@ -0,0 +1,92 @@
+
+# Copy over the 14B-088 data to be released with the first M33 HI paper
+
+getcert
+
+DR1_PATH="vos:ekoch/M33/VLA/14B-088/DR1/"
+
+DATA_PATH="/mnt/bigdata/ekoch/M33/VLA/14B-088/HI/full_imaging_wGBT"
+DATA_PATH_noSD="/mnt/bigdata/ekoch/M33/VLA/14B-088/HI/full_imaging_noSD"
+
+# HI Data products
+DR1_HI_PATH=$DR1_PATH/HI/
+vmkdir DR1_HI_PATH
+
+# 2.6 km/s channel version of the cube
+vcp $DATA_PATH/downsamp_to_co/M33_14B-088_HI.clean.image.GBT_feathered.2.6kms.fits $DR1_HI_PATH
+
+# Primary beam coverage
+vcp $DATA_PATH_noSD/M33_14B-088_pbcov.fits $DR1_HI_PATH
+
+# Moment maps
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.kurtosis.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.lwidth.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.mom0.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.mom1.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.peaktemps.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.peakvels.fits $DR1_HI_PATH
+vcp $DATA_PATH/M33_14B-088_HI.clean.image.GBT_feathered.pbcov_gt_0.5_masked.skewness.fits $DR1_HI_PATH
+
+# Stacked spectra
+vmkdir $DR1_HI_PATH/stacked_spectra
+
+vcp $DATA_PATH/stacked_spectra/centroid_stacked.fits $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH/stacked_spectra/rotation_stacked.fits $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH/stacked_spectra/peakvel_stacked.fits $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH/stacked_spectra/*100pc*.fits $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH/stacked_spectra/*5percentile*.fits $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH/stacked_spectra/peak_stacking_5percentile_num_pix.npy $DR1_HI_PATH/stacked_spectra/
+
+# Fits to the stacked spectra
+
+vcp $DATA_PATH/tables/hi_hwhm_totalprof_fits_peak_5percentile_feather.csv $DR1_HI_PATH/stacked_spectra/
+
+vcp $DATA_PATH_noSD/tables/hi_gaussian_hwhm_totalprof_fits_radial.csv $DR1_HI_PATH/stacked_spectra/
+vcp $DATA_PATH_noSD/tables/hi_gaussian_totalprof_hwhm_fits.csv $DR1_HI_PATH/stacked_spectra/
+
+# Rotation curve
+
+vmkdir $DR1_HI_PATH/rotation_curve
+
+vcp $DATA_PATH/diskfit_peakvels_noasymm_noradial_nowarp_output/diskfit_params_peakvels_nowarp_noradial_noasymm.inp $DR1_HI_PATH/rotation_curve/
+vcp $DATA_PATH/diskfit_peakvels_noasymm_noradial_nowarp_output/rad.out* $DR1_HI_PATH/rotation_curve/
+vcp $DATA_PATH/diskfit_peakvels_noasymm_noradial_nowarp_output/rad.fit*.*fits $DR1_HI_PATH/rotation_curve/
+vcp $DATA_PATH/diskfit_peakvels_noasymm_noradial_nowarp_output/rad.mod.fits $DR1_HI_PATH/rotation_curve/
+vcp $DATA_PATH/diskfit_peakvels_noasymm_noradial_nowarp_output/rad.res.fits $DR1_HI_PATH/rotation_curve/
+
+# OH cubes
+
+DR1_OH_PATH=$DR1_PATH/OH/
+vmkdir $DR1_OH_PATH
+
+OH_DATA_PATH="/mnt/bigdata/ekoch/VLA/14B-088/Lines/OH"
+
+vcp $OH_DATA_PATH/OH1612/imaging_1point5km_s/OH1612_14B-088_natural.image.pbcor.fits $DR1_OH_PATH
+vcp $OH_DATA_PATH/OH1665/imaging_1point5km_s/OH1665_14B-088_natural.image.pbcor.fits $DR1_OH_PATH
+vcp $OH_DATA_PATH/OH1667/imaging_1point5km_s/OH1667_14B-088_natural.image.pbcor.fits $DR1_OH_PATH
+vcp $OH_DATA_PATH/OH1720/imaging_1point5km_s/OH1720_14B-088_natural.image.pbcor.fits $DR1_OH_PATH
+
+
+# RRL cubes
+
+DR1_RRL_PATH=$DR1_PATH/RRL/
+vmkdir $DR1_RRL_PATH
+
+RRL_DATA_PATH="/mnt/bigdata/ekoch/VLA/14B-088/Lines/RRLs"
+
+vcp $RRL_DATA_PATH/combined_imaging/HRL_14B-088_combined.fits $DR1_RRL_PATH
+
+vcp $RRL_DATA_PATH/H152alp/dirty_images/H152alp_14B-088_dirty.image.fits $DR1_RRL_PATH
+vcp $RRL_DATA_PATH/H152alp/dirty_images/H152alp_14B-088_dirty.image.smooth.fits $DR1_RRL_PATH
+
+vcp $RRL_DATA_PATH/H153alp/dirty_images/H153alp_14B-088_dirty.image.fits $DR1_RRL_PATH
+vcp $RRL_DATA_PATH/H153alp/dirty_images/H153alp_14B-088_dirty.image.smooth.fits $DR1_RRL_PATH
+
+vcp $RRL_DATA_PATH/H158alp/dirty_images/H158alp_14B-088_dirty.image.fits $DR1_RRL_PATH
+vcp $RRL_DATA_PATH/H158alp/dirty_images/H158alp_14B-088_dirty.image.smooth.fits $DR1_RRL_PATH
+
+vcp $RRL_DATA_PATH/H164alp/dirty_images/H164alp_14B-088_dirty.image.fits $DR1_RRL_PATH
+vcp $RRL_DATA_PATH/H164alp/dirty_images/H164alp_14B-088_dirty.image.smooth.fits $DR1_RRL_PATH
+
+vcp $RRL_DATA_PATH/H166alp/dirty_images/H166alp_14B-088_dirty.image.fits $DR1_RRL_PATH
+vcp $RRL_DATA_PATH/H166alp/dirty_images/H166alp_14B-088_dirty.image.smooth.fits $DR1_RRL_PATH

48ec79e02efcd5cf076f969c8c6df58c2999c448
e-koch <koch.eric.w@gmail.com>
2018-05-03 20:13:32 -0600

    Adjustments to job script

--- 14B-088/HI/turbulence/M33_turbstats.slurm.sh
+++ 14B-088/HI/turbulence/M33_turbstats.slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --job-name=M33_turbulence
 #SBATCH --output=m33_turbulence-%J.out
-#SBATCH --acount=rrg-eros-ab
+#SBATCH --acount=def-eros-ab
 
 export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
 
@@ -17,8 +17,10 @@ module load fftw-mpi/3.3.6
 source /home/ekoch/.bashrc
 source /home/ekoch/preload.bash
 
-export scratch_path=/home/ekoch/scratch/14B-088
+export scratch_path=/home/ekoch/scratch/M33_turbulence
 export project_path=/home/ekoch/project/ekoch/
+
+cd $HOME
 
 # Call script with number of cores
 $HOME/anaconda3/bin/python $HOME/code/VLA_Lband/14B-088/turbulence/M33_turbstats.py 32

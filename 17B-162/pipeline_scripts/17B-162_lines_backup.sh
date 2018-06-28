5362be177e090289d4e83038b4501d540b6b33ed
e-koch <koch.eric.w@gmail.com>
2018-06-28 13:04:26 -0600

    Scripts to save needed pipeline outputs for the line tracks

---
+++ 17B-162/pipeline_scripts/17B-162_lines_backup.sh
@@ -0,0 +1,61 @@
+
+# Extract the necessary products for reproducing the calibration
+# from the 17B-162 line tracks
+
+# Make path prior to running script
+out_path='/home/ekoch/work2/ekoch/VLA_backups/17B-162/Lines'
+
+cd /home/ekoch/space/ekoch/VLA_tracks/17B-162/calibrated/Lines
+
+# Create a final flag version, then copy all of the necessary pipeline
+# outputs to the backup location
+# python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_09_23_17_speclines $out_path/17B-162_09_23_17_speclines T
+# rm -r $out_path/17B-162_09_23_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_09_26_17_speclines $out_path/17B-162_09_26_17_speclines T
+# rm -r $out_path/17B-162_09_26_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_16_17_speclines $out_path/17B-162_10_16_17_speclines T
+# rm -r $out_path/17B-162_10_16_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_17_17_early_speclines $out_path/17B-162_10_17_17_early_speclines T
+# rm -r $out_path/17B-162_10_17_17_early_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_17_17_late_speclines $out_path/17B-162_10_17_17_late_speclines T
+# rm -r $out_path/17B-162_10_17_17_late_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_19_17_speclines $out_path/17B-162_10_19_17_speclines T
+# rm -r $out_path/17B-162_10_19_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_21_17_speclines $out_path/17B-162_10_21_17_speclines T
+# rm -r $out_path/17B-162_10_21_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_22_17_early_speclines $out_path/17B-162_10_22_17_early_speclines T
+# rm -r $out_path/17B-162_10_22_17_early_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_22_17_late_speclines $out_path/17B-162_10_22_17_late_speclines T
+# rm -r $out_path/17B-162_10_22_17_late_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_26_17_speclines $out_path/17B-162_10_26_17_speclines T
+# rm -r $out_path/17B-162_10_26_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_28_17_speclines $out_path/17B-162_10_28_17_speclines T
+# rm -r $out_path/17B-162_10_28_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_29_17_speclines $out_path/17B-162_10_29_17_speclines T
+# rm -r $out_path/17B-162_10_29_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_30_17_speclines $out_path/17B-162_10_30_17_speclines T
+# rm -r $out_path/17B-162_10_30_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_31_17_speclines $out_path/17B-162_10_31_17_speclines T
+# rm -r $out_path/17B-162_10_31_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_11_19_17_speclines $out_path/17B-162_11_19_17_speclines T
+# rm -r $out_path/17B-162_11_19_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_11_25_17_speclines $out_path/17B-162_11_25_17_speclines T
+# rm -r $out_path/17B-162_11_25_17_speclines
+
+python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_12_01_17_speclines $out_path/17B-162_12_01_17_speclines T
+# rm -r $out_path/17B-162_12_01_17_speclines

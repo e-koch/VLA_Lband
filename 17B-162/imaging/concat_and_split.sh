a7c2dde1620eb6bb0ba5b07e69da2e279d496933
e-koch <koch.eric.w@gmail.com>
2018-06-30 20:48:22 -0600

    Need to switch directories

--- 17B-162/imaging/concat_and_split.sh
+++ 17B-162/imaging/concat_and_split.sh
@@ -13,4 +13,12 @@ module restore my_default
 source /home/ekoch/.bashrc
 source /home/ekoch/preload.bash
 
+export scratch_path=/home/ekoch/scratch/17B-162_imaging/
+
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
+Xvfb :1 &
+export DISPLAY=:1
+
 $HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/17B-162/imaging/concat_and_split.py

14cc94b0cdb2c3edb7f3d4ae91c62b3b2c45f5ca
e-koch <koch.eric.w@gmail.com>
2018-07-06 14:04:05 -0600

    Alter imaging script

--- 17B-162/HI/imaging/HI_single_channel_clean.py
+++ 17B-162/HI/imaging/HI_single_channel_clean.py
@@ -4,59 +4,62 @@ import sys
 import os
 
 # from casa_tools import myclean
-from tasks import clean, feather, rmtables
+from tasks import tclean
 
 '''
-Cleans an MS with a single channel given a mask and a model.
+Cleans a single channel given the channel name
 
 This is the template file. There are too many variables to easily generalize.
 '''
 
-vis = sys.argv[-3]
-model = sys.argv[-2]
-mask = sys.argv[-1]
+# Load in the SPW dict in the repo on cedar
+execfile(os.path.expanduser("~/code/VLA_Lband/17B-162/spw_setup.py"))
 
-if model == "None":
-    model = None
-if mask == "None":
-    mask = None
+chan_num = int(sys.argv[-1])
 
-out_root = vis[:-3]
+spw_num = 0
 
-# Define a valid mask. This stays true so long as a given mask contains a
-# usable region.
-valid_mask = True
-if mask is not None:
-    # Check if there's anything in the mask before cleaning
-    ia.open(mask)
-    stats_dict = ia.statistics()
-    ia.close()
-    # If there's nothing there, max == min
-    max_val = stats_dict["max"]
-    if max_val == 0:
-        casalog.post("No valid region in the given mask. Skipping clean.")
-        valid_mask = False
-    else:
-        casalog.post("Mask contains valid region. Proceeding with clean.")
+output_path = "spw_0_perchan/channel_{}".format(chan_num)
 
-if valid_mask:
-    clean(vis=vis, imagename=out_root + '.clean', field='M33*',
-          restfreq='1420.40575177MHz',
-          mode='channel', width=1, nchan=1, start=1,
-          cell='3arcsec', multiscale=[0, 4, 8, 20, 40, 80],
-          threshold='1.8mJy/beam', imagermode='mosaic', gain=0.1,
-          imsize=[2560, 2560], weighting='natural', robust=0.0, niter=200000,
-          pbcor=True, minpb=0.2, interpolation='linear', usescratch=False,
-          phasecenter='J2000 01h33m50.904 +30d39m35.79', veltype='radio',
-          modelimage=model, mask=mask)
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
 
-    # Only run if the image was actually produces
-    if os.path.exists(out_root + ".clean.image"):
-        feather(imagename=out_root + ".clean.image.feathered",
-                highres=out_root + ".clean.image",
-                lowres=model)
+mycellsize = '1.0arcsec'
 
-    # If something went awry, and the image wasn't produced, remove the
-    # remnants.
-    if not os.path.exists(out_root + ".clean.image"):
-        rmtables(out_root + ".clean.*")
+myimagesize = [6250, 6144]
+
+mypblimit = 0.1
+
+default('tclean')
+
+tclean(vis="17B-162_lines.ms",
+       datacolumn='corrected',
+       imagename=os.path.join(output_path,
+                              'M33_17B-162_{0}_channel_{1}.dirty'
+                              .format(linespw_dict[spw_num][0], chan_num)),
+       spw=str(spw_num),
+       field='M33*',
+       imsize=myimagesize,
+       cell=mycellsize,
+       specmode='cube',
+       start=chan_num,
+       width=1,
+       nchan=1,
+       startmodel=None,
+       gridder='mosaic',
+       weighting='natural',
+       niter=0,
+       threshold='3.2mJy/beam',
+       phasecenter='J2000 01h33m50.904 +30d39m35.79',
+       restfreq=linespw_dict[spw_num][1],
+       outframe='LSRK',
+       pblimit=mypblimit,
+       usemask='pb',
+       mask=None,
+       deconvolver='hogbom',
+       pbcor=False,
+       veltype='radio',
+       chanchunks=-1,
+       restoration=False,
+       parallel=False,
+       )

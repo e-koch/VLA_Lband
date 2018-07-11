b65728bd7732aa40b9b3f55f16f14c3faaf9710d
e-koch <koch.eric.w@gmail.com>
2018-07-10 21:51:10 -0600

    Simplify the pipeline backups for 17B

--- 17B-162/pipeline_scripts/17B-162_lines_backup.sh
+++ 17B-162/pipeline_scripts/17B-162_lines_backup.sh
@@ -7,55 +7,20 @@ out_path='/home/ekoch/work2/ekoch/VLA_backups/17B-162/Lines'
 
 cd /home/ekoch/space/ekoch/VLA_tracks/17B-162/calibrated/Lines
 
-# Create a final flag version, then copy all of the necessary pipeline
-# outputs to the backup location
-# python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_09_23_17_speclines $out_path/17B-162_09_23_17_speclines T
-# rm -r $out_path/17B-162_09_23_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_09_26_17_speclines $out_path/17B-162_09_26_17_speclines T
-# rm -r $out_path/17B-162_09_26_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_16_17_speclines $out_path/17B-162_10_16_17_speclines T
-# rm -r $out_path/17B-162_10_16_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_17_17_early_speclines $out_path/17B-162_10_17_17_early_speclines T
-# rm -r $out_path/17B-162_10_17_17_early_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_17_17_late_speclines $out_path/17B-162_10_17_17_late_speclines T
-# rm -r $out_path/17B-162_10_17_17_late_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_19_17_speclines $out_path/17B-162_10_19_17_speclines T
-# rm -r $out_path/17B-162_10_19_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_21_17_speclines $out_path/17B-162_10_21_17_speclines T
-# rm -r $out_path/17B-162_10_21_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_22_17_early_speclines $out_path/17B-162_10_22_17_early_speclines T
-# rm -r $out_path/17B-162_10_22_17_early_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_22_17_late_speclines $out_path/17B-162_10_22_17_late_speclines T
-# rm -r $out_path/17B-162_10_22_17_late_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_26_17_speclines $out_path/17B-162_10_26_17_speclines T
-# rm -r $out_path/17B-162_10_26_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_28_17_speclines $out_path/17B-162_10_28_17_speclines T
-# rm -r $out_path/17B-162_10_28_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_29_17_speclines $out_path/17B-162_10_29_17_speclines T
-# rm -r $out_path/17B-162_10_29_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_30_17_speclines $out_path/17B-162_10_30_17_speclines T
-# rm -r $out_path/17B-162_10_30_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_10_31_17_speclines $out_path/17B-162_10_31_17_speclines T
-# rm -r $out_path/17B-162_10_31_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_11_19_17_speclines $out_path/17B-162_11_19_17_speclines T
-# rm -r $out_path/17B-162_11_19_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_11_25_17_speclines $out_path/17B-162_11_25_17_speclines T
-# rm -r $out_path/17B-162_11_25_17_speclines
-
-python ~/Dropbox/code_development/VLA_Lband/17B-162/pipeline_scripts/pipeline_product_backup.py 17B-162_12_01_17_speclines $out_path/17B-162_12_01_17_speclines T
-# rm -r $out_path/17B-162_12_01_17_speclines
+mv 17B-162_09_23_17_speclines/products $out_path/17B-162_09_23_17
+mv 17B-162_09_26_17_speclines/products $out_path/17B-162_09_26_17
+mv 17B-162_10_16_17_speclines/products $out_path/17B-162_10_16_17
+mv 17B-162_10_17_17_early_speclines/products $out_path/17B-162_10_17_17_early
+mv 17B-162_10_17_17_late_speclines/products $out_path/17B-162_10_17_17_late
+mv 17B-162_10_19_17_speclines/products $out_path/17B-162_10_19_17
+mv 17B-162_10_21_17_speclines/products $out_path/17B-162_10_21_17
+mv 17B-162_10_22_17_early_speclines/products $out_path/17B-162_10_22_17_early
+mv 17B-162_10_22_17_late_speclines/products $out_path/17B-162_10_22_17_late
+mv 17B-162_10_26_17_speclines/products $out_path/17B-162_10_26_17
+mv 17B-162_10_28_17_speclines/products $out_path/17B-162_10_28_17
+mv 17B-162_10_29_17_speclines/products $out_path/17B-162_10_29_17
+mv 17B-162_10_30_17_speclines/products $out_path/17B-162_10_30_17
+mv 17B-162_10_31_17_speclines/products $out_path/17B-162_10_31_17
+mv 17B-162_11_19_17_speclines/products $out_path/17B-162_11_19_17
+mv 17B-162_11_25_17_speclines/products $out_path/17B-162_11_25_17
+mv 17B-162_12_01_17_speclines/products $out_path/17B-162_12_01_17

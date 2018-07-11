9e222f76033f1891a8d2064ceb7bd3dd2a3f5306
e-koch <koch.eric.w@gmail.com>
2018-07-11 14:04:40 -0600

    Regrid and combine 14B and 17B

---
+++ 17B-162/HI/imaging/match_and_combine.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --time=60:00:00
+#SBATCH --mem=1547000M
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --job-name=M33_HI_match_and_combine-%J
+#SBATCH --output=casa-m33_HI_match_and_combine-%J.out
+
+# Create a combined 14B and 17B HI MMS
+
+export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+
+module restore my_default
+
+source /home/ekoch/.bashrc
+source /home/ekoch/preload.bash
+
+export scratch_path=/home/ekoch/scratch/17B-162_imaging/
+
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
+Xvfb :1 &
+export DISPLAY=:1
+
+$HOME/casa-release-5.3.0-143.el7/bin/mpicasa -n 32 $HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/17B-162/HI/imaging/match_and_combine.py

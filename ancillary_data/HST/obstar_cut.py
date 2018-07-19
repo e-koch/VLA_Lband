3aadf67f8962d9e8d5364cac09a9a9fafe3ce555
e-koch <koch.eric.w@gmail.com>
2018-07-19 08:55:48 +0200

    Fix code for working in py27 (presumably?) and give some better names

--- ancillary_data/HST/obstar_cut.py
+++ ancillary_data/HST/obstar_cut.py
@@ -11,11 +11,15 @@ df_phot = pd.read_hdf(hst_phat_path("halfbrick_6filt_phot/14610_M33-B01_1.phot.h
 n_gst = df_phot.filter(regex='_gst').sum(axis=1)
 n_gst.head()
 
-df_good = df[n_gst >= 4]
+df_good = df_phot[n_gst >= 4]
 print('Number of stars with 4+ GST measurements:', df_good.shape[0])
-# Make cuts
-obstars = df_phot.query('(f336w_f475w < -0.5) & (f336w_vega < 18.12) & (f336w_f475w > -2) & ()')
 
-obstars.to_hdf(hst_phat_path('halfbrick_6filt_phot/14610_M33-B01_1.phot.OBcut.hdf5',
-                             no_check=False),
-               key='data', mode='w')
+# Define some colour columns
+df_good = df_good.assign(f336w_f475w_vega = df_good.f336w_vega - df_good.f475w_vega)
+
+# Make cuts
+ostars = df_good.query('(f336w_f475w_vega < -0.5) & (f336w_vega < 18.12) & (f336w_f475w_vega > -2)')
+
+ostars.to_hdf(hst_phat_path('halfbrick_6filt_phot/14610_M33-B01_1.phot.Ocut.hdf5',
+                            no_check=True),
+              key='data', mode='w')

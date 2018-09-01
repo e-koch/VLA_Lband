96cccb8b45ec1cd813f7f433a2916c219eb6dc96
e-koch <koch.eric.w@gmail.com>
2018-09-01 08:58:22 -0700

    Set path for logfile

--- 17B-162/HI/imaging/HI_imaging_array_submission_02kms.sh
+++ 17B-162/HI/imaging/HI_imaging_array_submission_02kms.sh
@@ -42,7 +42,7 @@ for (( chan_num = $start_chan; chan_num < $end_chan; chan_num++ )); do
 
     echo "Running channel "$chan_num
 
-    $HOME/casa-release-5.3.0-143.el7/bin/mpicasa -n 8 $HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --logfile casa_M33_HI_14B_17B_02kms_${chan_num}_${SLURM_JOB_ID}_$(date "+%Y%m%d-%H%M%S").log --nocrashreport -c $HOME/code/VLA_Lband/17B-162/HI/imaging/HI_single_channel_clean.py $chan_num $param_file "HI_contsub_021kms" &
+    $HOME/casa-release-5.3.0-143.el7/bin/mpicasa -n 8 $HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --logfile $scratch_path/HI_contsub_021kms/casa_M33_HI_14B_17B_02kms_${chan_num}_${SLURM_JOB_ID}_$(date "+%Y%m%d-%H%M%S").log --nocrashreport -c $HOME/code/VLA_Lband/17B-162/HI/imaging/HI_single_channel_clean.py $chan_num $param_file "HI_contsub_021kms" &
     pids+=" $!"
 
 done

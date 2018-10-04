1b8aaf4d551e27bbde8229ee25e76edfdf61e92d
e-koch <koch.eric.w@gmail.com>
2018-10-04 16:24:36 -0600

    Add estimates of std between HI and CO velocities at lower resolution (this may already be in another script...)

--- 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
+++ 14B-088/HI/analysis/co_comparison/co_hi_veloffset.py
@@ -10,9 +10,11 @@ from os.path import join, exists
 import os
 
 from paths import (fourteenB_wGBT_HI_file_dict,
+                   fourteenB_HI_data_wGBT_path,
                    iram_co21_14B088_data_path,
                    allfigs_path)
-from plotting_styles import onecolumn_figure, default_figure, twocolumn_figure
+from plotting_styles import (onecolumn_figure, default_figure,
+                             twocolumn_figure)
 from constants import hi_freq
 
 
@@ -199,6 +201,57 @@ cbar.set_label(r"Integrated Intensity (K km s$^{-1}$)")
 
 plt.savefig(allfigs_path(join(fig_path, "co21_HI_peakvels_offset_outliers.pdf")))
 plt.savefig(allfigs_path(join(fig_path, "co21_HI_peakvels_offset_outliers.png")))
+plt.close()
+
+# Compare the velocity difference distributions at coarser resolution
+
+hi_peakvels_38 = fits.open(fourteenB_HI_data_wGBT_path("smooth_2beam/M33_14B-088_HI.clean.image.GBT_feathered.38arcsec.peakvels.fits"))[0].data / 1000.
+co_peakvels_38 = fits.open(iram_co21_14B088_data_path("smooth_2beam/m33.co21_iram.14B-088_HI.38arcsec.peakvels.fits"))[0].data / 1000.
+
+good_pts_38 = np.logical_and(np.isfinite(hi_peakvels_38), np.isfinite(co_peakvels_38))
+good_pts_38 = np.logical_and(good_pts_38, co_peaktemp > 0.06)
+
+
+hi_peakvels_95 = fits.open(fourteenB_HI_data_wGBT_path("smooth_5beam/M33_14B-088_HI.clean.image.GBT_feathered.95arcsec.peakvels.fits"))[0].data / 1000.
+co_peakvels_95 = fits.open(iram_co21_14B088_data_path("smooth_5beam/m33.co21_iram.14B-088_HI.95arcsec.peakvels.fits"))[0].data / 1000.
+
+good_pts_95 = np.logical_and(np.isfinite(hi_peakvels_95), np.isfinite(co_peakvels_95))
+good_pts_95 = np.logical_and(good_pts_95, co_peaktemp > 0.06)
+
+onecolumn_figure()
+
+peakvel_diff = (co_peakvels - hi_peakvels)[good_pts]
+_ = plt.hist(peakvel_diff, bins='auto', label='19"', alpha=0.6)
+
+peakvel_diff_38 = (co_peakvels_38 - hi_peakvels_38)[good_pts_38]
+_ = plt.hist(peakvel_diff_38, bins='auto', label='38"', alpha=0.6)
+
+peakvel_diff_95 = (co_peakvels_95 - hi_peakvels_95)[good_pts_95]
+_ = plt.hist(peakvel_diff_95, bins='auto', label='95"', alpha=0.6)
+
+plt.legend(frameon=True)
+plt.axvline(2.6, color=cpal[2], linestyle=':', linewidth=3)
+plt.axvline(-2.6, color=cpal[2], linestyle=':', linewidth=3)
+plt.xlabel(r"$V_{\rm peak, CO} - V_{\rm peak, HI}$ (km/s)")
+plt.grid()
+plt.tight_layout()
+
+print("Std at 19: {}".format(np.std(peakvel_diff[np.abs(peakvel_diff) < 10])))
+print("Std at 38: {}".format(np.std(peakvel_diff_38[np.abs(peakvel_diff_38) < 10])))
+print("Std at 95: {}".format(np.std(peakvel_diff_95[np.abs(peakvel_diff_95) < 10])))
+# Std at 19: 2.76193498451
+# Std at 38: 3.09502172119
+# Std at 95: 3.28943228722
+
+print("Fraction of outliers beyond ~4 sigma at 19: {}".format((np.abs(peakvel_diff) > 10).sum() / float(peakvel_diff.size)))
+print("Fraction of outliers beyond ~4 sigma at 38: {}".format((np.abs(peakvel_diff_38) > 10).sum() / float(peakvel_diff_38.size)))
+print("Fraction of outliers beyond ~4 sigma at 95: {}".format((np.abs(peakvel_diff_95) > 10).sum() / float(peakvel_diff_95.size)))
+# Fraction of outliers beyond ~4 sigma at 19: 0.0260891034785
+# Fraction of outliers beyond ~4 sigma at 38: 0.0169904255908
+# Fraction of outliers beyond ~4 sigma at 95: 0.0414575666332
+
+plt.savefig(allfigs_path(join(fig_path, "co21_hi_peakvel_velocity_offset_rescompare.pdf")))
+plt.savefig(allfigs_path(join(fig_path, "co21_hi_peakvel_velocity_offset_rescompare.png")))
 plt.close()
 
 default_figure()

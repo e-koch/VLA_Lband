0f1368f12fd53d7a22c5441a33bc4cea2b2824f6
e-koch <koch.eric.w@gmail.com>
2018-10-06 22:45:34 -0600

    Backup pipeline products for line SPWs

---
+++ 16B/16B-236/16B-236_lines_backup.sh
@@ -0,0 +1,21 @@
+
+# Extract the necessary products for reproducing the calibration
+# from the 16B-236 line tracks
+
+# Make path prior to running script
+out_path='/home/ekoch/work2/ekoch/VLA_backups/16B-236/Lines'
+
+cd /home/ekoch/space/ekoch/VLA_tracks/16B-236/calibrated/
+
+mv 16B-236_11_11_16_speclines/products $out_path/16B-236_11_11_16
+mv 16B-236_11_16_16_speclines/products $out_path/16B-236_11_16_16
+mv 16B-236_11_26_16_speclines/products $out_path/16B-236_11_26_16
+mv 16B-236_11_27_16_speclines/products $out_path/16B-236_11_27_16
+mv 16B-236_11_28_16_speclines/products $out_path/16B-236_11_28_16
+mv 16B-236_11_29_16_speclines/products $out_path/16B-236_11_29_16
+mv 16B-236_12_08_16_speclines/products $out_path/16B-236_12_08_16
+mv 16B-236_12_09_16_speclines/products $out_path/16B-236_12_09_16
+mv 16B-236_12_15_16_speclines/products $out_path/16B-236_12_15_16
+mv 16B-236_12_17_16_speclines/products $out_path/16B-236_12_17_16
+mv 16B-236_12_30_16_speclines/products $out_path/16B-236_12_30_16
+mv 16B-236_01_01_17_speclines/products $out_path/16B-236_01_01_17

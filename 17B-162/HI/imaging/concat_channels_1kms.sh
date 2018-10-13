5503fcf9eb88ed38c9e36c75e20f5ddf344bc6a3
e-koch <koch.eric.w@gmail.com>
2018-10-13 17:42:20 -0600

    Add concat job for 1kms cube

---
+++ 17B-162/HI/imaging/concat_channels_1kms.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --mem=128000M
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --job-name=M33_HI_concat_channel-1kms-%A-%a
+#SBATCH --output=casa-m33_HI_concat_channel-1kms-%A-%a.out
+#SBATCH --array=0-9
+
+# Combine the imaged channels into cubes
+# Run on whole node for the memory.
+
+# export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+
+module restore my_default
+
+source /home/ekoch/.bashrc
+source /home/ekoch/preload.bash
+
+export scratch_path=/home/ekoch/scratch/17B-162_imaging/
+
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
+Xvfb :1 &
+export DISPLAY=:1
+
+suffixes="mask model pb psf residual residual_init image sumwt weight"
+suffix_arr=($suffixes)
+
+job_num=$SLURM_ARRAY_TASK_ID
+
+$HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/17B-162/HI/imaging/concat_channels.py "HI_contsub_10kms" "M33_14B_17B_HI_contsub_width_1kms" 280 ${suffix_arr[$job_num]}

797a54f93408b1f2dced979ea17746776fc54f22
e-koch <koch.eric.w@gmail.com>
2018-10-24 10:24:16 -0600

    Transform and uvsub for 236

---
+++ 16B/16B-236/imaging/transform_and_uvsub.py
@@ -0,0 +1,64 @@
+
+'''
+Split out each SPW from the combined MS (concat_and_split.py), convert
+to LSRK, and subtract continuum in uv-plane
+'''
+
+import os
+import sys
+
+from tasks import mstransform, uvcontsub, partition, split
+
+myvis = '16B-236_lines.ms'
+
+spw_num = int(sys.argv[-1])
+
+# Load in the SPW dict in the repo on cedar
+execfile(os.path.expanduser("~/code/VLA_Lband/16B/spw_setup.py"))
+
+
+default('mstransform')
+
+casalog.post("On SPW {}".format(spw_num))
+
+# Note that the combined MS already only includes the calibrated data
+# with all flagged data removed.
+
+# If this is HI, we want to keep the continuum version to look for
+# absorption features. Split the uvsubtraction into a separate function
+
+out_vis = "16B-236_{0}_spw_{1}_LSRK.ms"\
+    .format(linespw_dict[spw_num][0], spw_num)
+
+mstransform(vis=myvis, outputvis=out_vis, spw=str(spw_num),
+            datacolumn='data',
+            regridms=True, mode='channel', interpolation='fftshift',
+            # phasecenter='J2000 01h33m50.904 +30d39m35.79',
+            restfreq=linespw_dict[spw_num][1], outframe='LSRK',
+            douvcontsub=False)
+
+# Separate uvcontsub
+
+out_vis_cs = "16B-236_{0}_spw_{1}_LSRK.ms.contsub"\
+    .format(linespw_dict[spw_num][0], spw_num)
+
+out_mms_vis_cs = "16B-236_{0}_spw_{1}_LSRK.mms.contsub"\
+     .format(linespw_dict[spw_num][0], spw_num)
+
+# The operation is much fast in parallel, so make an MMS and then
+# convert back
+default('partition')
+partition(vis=out_vis, outputvis=out_vis[:-3] + ".mms", createmms=True,
+          separationaxis='auto', flagbackup=False)
+
+default('uvcontsub')
+uvcontsub(vis=out_vis[:-3] + ".mms",
+          fitspw='{0}:{1}'.format(spw_num,
+                                  linespw_236_uvsub[spw_num]),
+          fitorder=0 if spw_num != 0 else 1,
+          want_cont=False)
+
+default('split')
+split(vis=out_mms_vis_cs, outputvis=out_vis_cs, keepmms=False)
+
+os.system("rm -r {}".format(out_mms_vis_cs))
---
+++ 16B/16B-236/imaging/transform_and_uvsub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --mem=515000M
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --job-name=M33_16B236_transform_and_uvsub-%A-%a
+#SBATCH --output=casa-m33_16B236_transform_and_uvsub-%A-%a.out
+#SBATCH --array=0-9%1
+
+export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+
+module restore my_default
+
+source /home/ekoch/.bashrc
+source /home/ekoch/preload.bash
+
+export scratch_path=/home/ekoch/scratch/16B-236_imaging/
+
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
+Xvfb :1 &
+export DISPLAY=:1
+
+spw=$SLURM_ARRAY_TASK_ID
+
+$HOME/casa-release-5.3.0-143.el7/bin/mpicasa -n 32 $HOME/casa-release-5.3.0-143.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/16B/16B-236/imaging/transform_and_uvsub.py $spw

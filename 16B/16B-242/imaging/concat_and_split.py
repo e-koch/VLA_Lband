0dccc39b406f661af73323186300f736d290759a
e-koch <koch.eric.w@gmail.com>
2019-04-23 10:23:58 -0400

    Avoid using virtualconcat

--- 16B/16B-242/imaging/concat_and_split.py
+++ 16B/16B-242/imaging/concat_and_split.py
@@ -6,16 +6,15 @@ Combine the tracks, then split out the science fields
 import os
 from glob import glob
 
-from tasks import virtualconcat, split
+from tasks import concat, split
 
 # Grab all of the MS tracks in the folder (should be 12)
 myvis = glob("16B-242.*.ms")
 
 assert len(myvis) == 12
 
-default('virtualconcat')
-virtualconcat(vis=myvis, concatvis='16B-242_lines_all.ms',
-              keepcopy=False)
+default('concat')
+concat(vis=myvis, concatvis='16B-242_lines_all.ms')
 
 default('split')
 split(vis='16B-242_lines_all.ms', outputvis='16B-242_lines.ms',

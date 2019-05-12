84f8630bc2744caa84ae66ca0f5c22a1e0878a24
e-koch <koch.eric.w@gmail.com>
2019-05-12 10:29:38 -0600

    Update 17B split and uvcontsub to not use an MMS

--- 17B-162/imaging/transform_and_uvsub.py
+++ 17B-162/imaging/transform_and_uvsub.py
@@ -5,60 +5,43 @@ to LSRK, and subtract continuum in uv-plane
 '''
 
 import os
+import sys
 
-from tasks import mstransform, uvcontsub, partition, split
+from tasks import mstransform, uvcontsub
 
 myvis = '17B-162_lines.ms'
+
+spw_num = int(sys.argv[-1])
 
 # Load in the SPW dict in the repo on cedar
 execfile(os.path.expanduser("~/code/VLA_Lband/17B-162/spw_setup.py"))
 
-for spw_num in range(0, 10):
 
-    default('mstransform')
+default('mstransform')
 
-    casalog.post("On SPW {}".format(spw_num))
+casalog.post("On SPW {}".format(spw_num))
 
-    # Note that the combined MS already only includes the calibrated data
-    # with all flagged data removed.
+# Note that the combined MS already only includes the calibrated data
+# with all flagged data removed.
 
-    # If this is HI, we want to keep the continuum version to look for
-    # absorption features. Split the uvsubtraction into a separate function
+out_vis = "17B-162_{1}_spw_{2}_LSRK.ms"\
+    .format(linespw_dict[spw_num][0], spw_num)
 
-    if spw_num == 0:
-        do_uvcontsub = False
-        out_vis = "17B-162_{0}_spw_{1}_LSRK.ms"\
-            .format(linespw_dict[spw_num][0], spw_num)
-    else:
-        do_uvcontsub = True
-        out_vis = "17B-162_{0}_spw_{1}_LSRK.ms.contsub"\
-            .format(linespw_dict[spw_num][0], spw_num)
 
-    mstransform(vis=myvis, outputvis=out_vis, spw=str(spw_num),
-                datacolumn='data',
-                regridms=True, mode='channel', interpolation='fftshift',
-                phasecenter='J2000 01h33m50.904 +30d39m35.79',
-                restfreq=linespw_dict[spw_num][1], outframe='LSRK',
-                douvcontsub=do_uvcontsub,
-                fitspw='{0}:{1}'.format(spw_num, linespw_dict[spw_num][3]),
-                fitorder=0 if spw_num != 0 else 1, want_cont=False)
+# in casa 5.4.1, fftshift does something that is not linear interpolation
+# but it gives severe edge effects when I've used it on the 13A-213 data
+mstransform(vis=myvis, outputvis=out_vis, spw=str(spw_num),
+            datacolumn='data',
+            field='M33*',
+            regridms=True, mode='channel',
+            interpolation='linear',  # 'fftshift',
+            phasecenter='J2000 01h33m50.904 +30d39m35.79',
+            restfreq=linespw_dict[spw_num][1], outframe='LSRK',
+            douvcontsub=False)
 
-    # Separate uvcontsub for HI
-    if spw_num == 0:
-        out_vis_cs = "17B-162_{0}_spw_{1}_LSRK.ms.contsub"\
-            .format(linespw_dict[spw_num][0], spw_num)
+default('uvcontsub')
 
-        out_mms_vis_cs = "17B-162_{0}_spw_{1}_LSRK.mms.contsub"\
-             .format(linespw_dict[spw_num][0], spw_num)
-
-        # The operation is much fast in parallel, so make an MMS and then
-        # convert back
-        partition(vis=out_vis, outputvis=out_vis[:-3] + ".mms", createmms=True,
-                  separationaxis='auto', flagbackup=False)
-
-        uvcontsub(vis=out_vis[:-3] + ".mms",
-                  fitspw='{0}:{1}'.format(spw_num, linespw_dict[spw_num][3]),
-                  fitorder=0, want_cont=False)
-
-        default('split')
-        split(vis=out_mms_vis_cs, outputvis=out_vis_cs, keepmms=False)
+# Separate uvcontsub call
+uvcontsub(vis=out_vis,
+          fitspw='{0}:{1}'.format(spw_num, linespw_dict[spw_num][3]),
+          fitorder=0, want_cont=False)
--- 17B-162/imaging/transform_and_uvsub.sh
+++ 17B-162/imaging/transform_and_uvsub.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --time=60:00:00
-#SBATCH --mem=515000M
+#SBATCH --time=20:00:00
+#SBATCH --mem=128000M
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
-#SBATCH --job-name=M33_transform_and_uvsub-%J
-#SBATCH --output=casa-m33_transform_and_uvsub-%J.out
+#SBATCH --job-name=M33_transform_and_uvsub-%A-%a
+#SBATCH --output=casa-m33_transform_and_uvsub-%A-%a.out
+#SBATCH --array=0-10%1
 
 export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
 
@@ -15,10 +16,13 @@ source /home/ekoch/preload.bash
 
 export scratch_path=/home/ekoch/scratch/17B-162_imaging/
 
-# Move to scratch space b/c casa wrqite out the temporary files into the same folder
+# Move to scratch space b/c casa write out the temporary files into the same folder
 cd $scratch_path
 
 Xvfb :1 &
 export DISPLAY=:1
 
-$HOME/casa-release-5.4.1-32.el7/bin/mpicasa -n 32 $HOME/casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/17B-162/imaging/transform_and_uvsub.py
+
+export spw=$SLURM_ARRAY_TASK_ID
+
+$HOME/casa-release-5.4.1-32.el7/bin/mpicasa -n 32 $HOME/casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/VLA_Lband/17B-162/imaging/transform_and_uvsub.py $spw

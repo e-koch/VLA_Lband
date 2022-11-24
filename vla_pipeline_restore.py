9a0a9643059230d374a2b30f6f2ff53a892b5818
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 02:30:18 +0000

    Adding tarfile member sanitization to extractall()

--- vla_pipeline_restore.py
+++ vla_pipeline_restore.py
@@ -55,7 +55,26 @@ try:
     if not os.path.exists(flagname):
         raise OSError("Cannot find flagversions.tgz: {}".format(flagname))
     with tarfile.open(flagname, 'r:gz') as tar:
-        tar.extractall(path="")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path="")
 
     # Assume this is the name for now. Should be fine for all single
     # track pipeline runs
@@ -63,7 +82,26 @@ try:
     if not os.path.exists(tablename):
         raise OSError("Cannot find caltables.tgz: {}".format(tablename))
     with tarfile.open(tablename, 'r:gz') as tar:
-        tar.extractall(path="")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path="")
 
     # Want to check if any custom flagging was made after
     # the `Pipeline_Final` version. If so, restore that version
